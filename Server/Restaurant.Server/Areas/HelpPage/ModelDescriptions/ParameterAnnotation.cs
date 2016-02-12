cc527e525614d1b4f4f5ded120b906201f41998a
jurabek <jurabek.azizkhujaev@gmail.com>
2016-02-13 00:16:00 +0500

    Init Commit

---
+++ Server/Restaurant.Server/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Restaurant.Server.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}

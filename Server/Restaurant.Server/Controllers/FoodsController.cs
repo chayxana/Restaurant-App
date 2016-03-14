eec1199e52a86f0482f7e153388965c4effa3904
jurabek <jurabeka@pm.me>
2016-03-14 20:24:44 +0500

    Added BaseApiController and FoodsController and some other name changes

---
+++ Server/Restaurant.Server/Controllers/FoodsController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Restaurant.Server.Models;
+
+namespace Restaurant.Server.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Food")]
+    public class FoodsController : BaseApiContoller
+    {
+        [Route("GetFoods")]
+        public IEnumerable<Food> GetFoods()
+        {
+            var foods = Context.Foods;
+            return foods;
+        }
+        [Route("AddFood")]
+        public IHttpActionResult AddFood(Food food)
+        {
+            try
+            {
+                if (Context.Foods.Add(food))
+                {
+                    Context.SaveChanges();
+                    return Ok(new ResultResponce { IsSucceeded = true });
+                }
+            }
+            catch (Exception)
+            {
+                Context.GetValidationErrors();
+            }
+
+            return InternalServerError();
+        }
+
+        [Route("DeleteFood")]
+        public IHttpActionResult DeleteFood(Food food)
+        {
+            try
+            {
+                bool result = Context.Foods.Remove(food);
+                if (result)
+                {
+                    Context.SaveChanges();
+                    return Ok(new ResultResponce { IsSucceeded = true });
+                }
+            }
+            catch (Exception)
+            {
+                Context.GetValidationErrors();
+            }
+
+            return InternalServerError();
+        }
+
+        [Route("UpdateFood")]
+        public IHttpActionResult UpdateFood(Food food)
+        {
+            try
+            {
+                bool result = Context.Foods.Remove(food);
+                if (result)
+                {
+                    Context.SaveChanges();
+                    return Ok(new ResultResponce { IsSucceeded = true });
+                }
+            }
+            catch (Exception)
+            {
+                Context.GetValidationErrors();
+            }
+
+            return InternalServerError();
+        }
+    }
+    internal class ResultResponce : IHttpActionResult
+    {
+        public bool IsSucceeded { get; set; }
+
+        public Dictionary<string, Exception> Errors { get; set; }
+
+        public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

fa8c27c6528da3247957b15030a06b0b7bf837e9
jurabek <jurabek.azizkhujaev@gmail.com>
2016-03-14 20:41:53 +0500

    Changed iOS Security connection and added Name into User

--- Client/Restaurant.Client/Restaurant/ViewModels/FoodsViewModel.cs
+++ Client/Restaurant.Client/Restaurant/ViewModels/FoodsViewModel.cs
@@ -22,7 +22,7 @@ namespace Restaurant.ViewModels
         {
             get
             {
-                throw new NotImplementedException();
+                return "Foods";
             }
         }
     }

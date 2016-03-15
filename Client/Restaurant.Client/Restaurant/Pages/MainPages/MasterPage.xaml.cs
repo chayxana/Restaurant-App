fde070c3fb1b6a84c62bee6ddc882fa484076e3f
jurabek <jurabek.azizkhujaev@gmail.com>
2016-03-15 16:07:08 +0500

    Fixed all UI parts added missed files

---
+++ Client/Restaurant.Client/Restaurant/Pages/MainPages/MasterPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Restaurant.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Restaurant.Pages.MainPages
+{
+    public partial class MasterPage : ContentPage
+    {
+        public MainViewModel ViewModel { get; set; }
+
+        public MasterPage(MainViewModel viewModel)
+        {
+            InitializeComponent();
+        }
+    }
+}

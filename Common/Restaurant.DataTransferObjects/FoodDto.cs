054762b47e4ea88d4b2deb546f835d785b4a19ec
jurabek <jurabeka@pm.me>
2017-05-25 13:23:44 +0300

    Food details page

--- Common/Restaurant.DataTransferObjects/FoodDto.cs
+++ Common/Restaurant.DataTransferObjects/FoodDto.cs
@@ -13,6 +13,8 @@ namespace Restaurant.DataTransferObjects
 
         public string Name { get; set; }
 
+        public string Description { get; set; }
+
         public string Picture { get; set; }
 
         public decimal Price { get; set; }

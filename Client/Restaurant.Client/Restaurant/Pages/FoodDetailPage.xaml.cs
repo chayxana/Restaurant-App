da8eb372092283eda433b8704a414b53c22531e3
jurabek <jurabeka@pm.me>
2017-06-05 14:39:34 +0300

    Food detailes page

--- Client/Restaurant.Client/Restaurant/Pages/FoodDetailPage.xaml.cs
+++ Client/Restaurant.Client/Restaurant/Pages/FoodDetailPage.xaml.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Restaurant.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Restaurant.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class FoodDetailPage : ContentPage
+    public partial class FoodDetailPage : FoodDetailPageXaml
     {
         public FoodDetailPage()
         {
             InitializeComponent();
         }
+
+        protected override void OnLoaded()
+        {
+
+        }
+    }
+
+    public abstract class FoodDetailPageXaml : BaseContentPage<FoodDetailViewModel>
+    {   
     }
 }

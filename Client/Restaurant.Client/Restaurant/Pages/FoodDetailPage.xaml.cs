65da027d3d4d9ad81d34a6071e26f6c31fca4abe
jurabek <jurabeka@pm.me>
2017-06-19 23:15:54 -0700

    Food changes

--- Client/Restaurant.Client/Restaurant/Pages/FoodDetailPage.xaml.cs
+++ Client/Restaurant.Client/Restaurant/Pages/FoodDetailPage.xaml.cs
@@ -12,6 +12,8 @@ namespace Restaurant.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FoodDetailPage : FoodDetailPageXaml
     {
+        private double _imageHeight;
+
         public FoodDetailPage()
         {
             InitializeComponent();
@@ -20,10 +22,34 @@ namespace Restaurant.Pages
         protected override void OnLoaded()
         {
             BindingContext = ViewModel;
+
+            scrollView.Scrolled += (sender, e) => Parallax();
+            Parallax();
+        }
+
+        void Parallax()
+        {
+            if (_imageHeight <= 0)
+                _imageHeight = photoImage.Height;
+
+            var y = scrollView.ScrollY * .4;
+            if (y >= 0)
+            {
+                //Move the Image's Y coordinate a fraction of the ScrollView's Y position
+                photoImage.Scale = 1;
+                photoImage.TranslationY = y;
+            }
+            else
+            {
+                //Calculate a scale that equalizes the height vs scroll
+                double newHeight = _imageHeight + (scrollView.ScrollY * -1);
+                photoImage.Scale = newHeight / _imageHeight;
+                photoImage.TranslationY = scrollView.ScrollY / 2;
+            }
         }
     }
 
     public abstract class FoodDetailPageXaml : BaseContentPage<FoodDetailViewModel>
-    {   
+    {
     }
 }

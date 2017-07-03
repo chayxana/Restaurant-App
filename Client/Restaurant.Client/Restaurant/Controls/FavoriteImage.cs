1ac162f116dc8bce1c80617b2aa3197aa6b37492
Jurabek Azizkhujaev <jurabek.azizkhujaev@hotmail.com>
2017-07-02 23:51:50 -0700

    view changes

---
+++ Client/Restaurant.Client/Restaurant/Controls/FavoriteImage.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Restaurant.Controls
+{
+    public class FavoriteImage : Image
+    {
+        bool addedAnimation;
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            if (addedAnimation || GestureRecognizers.Count == 0)
+                return;
+
+            addedAnimation = true;
+
+            var tapGesture = GestureRecognizers[0] as TapGestureRecognizer;
+            if (tapGesture == null)
+                return;
+
+            tapGesture.Tapped += (sender, e) =>
+            {
+                Device.BeginInvokeOnMainThread(() => Grow());
+            };
+
+        }
+
+        /// <summary>
+        /// Play animation to grow and shrink
+        /// </summary>
+        public async Task Grow()
+        {
+
+            await this.ScaleTo(1.4, 75);
+            await this.ScaleTo(1.0, 75);
+
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                try
+                {
+                    this.ScaleTo(1.4, 75).ContinueWith((t) =>
+                        {
+                            try
+                            {
+                                this.ScaleTo(1.0, 75);
+                            }
+                            catch
+                            {
+                            }
+                        },
+                        scheduler: TaskScheduler.FromCurrentSynchronizationContext());
+                }
+                catch
+                {
+                }
+            });
+        }
+    }
+}
+

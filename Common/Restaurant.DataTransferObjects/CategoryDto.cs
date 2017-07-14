4a5532710843e344819457b1643eb4492c5f5ccf
Azizkhujaev, Jurabek <jurabek.azizkhujaev@gmail.com>
2017-07-14 14:35:05 +0300

    Notification Dto

---
+++ Common/Restaurant.DataTransferObjects/CategoryDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.DataTransferObjects
+{
+    public class CategoryDto
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IEnumerable<FoodDto> Foods { get; set; }
+    }
+}

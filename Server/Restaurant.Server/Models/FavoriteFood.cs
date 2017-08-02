b69ba6c1acd7fada6f076e28cd6c252dc8e69a28
Azizkhujaev, Jurabek <jurabek.azizkhujaev@gmail.com>
2017-08-02 16:46:28 +0300

    angular services

---
+++ Server/Restaurant.Server/Models/FavoriteFood.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Restaurant.Server.Models
+{
+    public class FavoriteFood : BaseEntity
+    {
+        public virtual Guid FoodId { get; set; }
+
+        public virtual string UserId { get; set; }
+    }
+}
---
+++ Web-Client/restaurant-manager/src/app/models/category.ts
@@ -0,0 +1,5 @@
+export class Category {
+    id: string;
+    name: string;
+    color: string;
+}

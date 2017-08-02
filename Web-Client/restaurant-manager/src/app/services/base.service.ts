e71110a839d0ffff1aeda02c2999fd6c68a8b520
jurabek <jurabeka@pm.me>
2017-08-02 17:32:36 +0300

    base service

--- Web-Client/restaurant-manager/src/app/services/base.service.ts
+++ Web-Client/restaurant-manager/src/app/services/base.service.ts
@@ -1,13 +1,54 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http'
+import { Observable } from 'rxjs/Observable';
 
-export abstract class BaseService {
-    protected baseUrl: string;
+export abstract class BaseService<T> {
     protected headers: Headers;
     protected options: RequestOptions;
 
-    constructor() {
+    constructor(private http: Http) {
         this.headers = new Headers({ 'Content-Type': 'application/json' });
         this.options = new RequestOptions({ headers: this.headers });
+    }
+
+    abstract baseUrl(): string;
+
+    getAll(): Observable<T[]> {
+        
+        return this.http.get(this.baseUrl())
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    get(id: string): Observable<T> {
+        return null;
+    }
+
+    update(model: T) {
+        return null;
+    }
+
+    create(model: T) {
+        return null;
+    }
+
+
+    public extractData(res: Response) {
+        let body = res.json();
+        return body || {};
+    }
+
+    public handleError(error: Response | any) {
+        // In a real world app, you might use a remote logging infrastructure
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || '';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
     }
 }
--- Web-Client/restaurant-manager/src/app/services/food.service.ts
+++ Web-Client/restaurant-manager/src/app/services/food.service.ts
@@ -2,31 +2,20 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { Food } from 'app/models/food'
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
 import { BaseService } from "app/services/base.service";
 
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
 @Injectable()
-export class FoodService extends BaseService {
+export class FoodService extends BaseService<Food> {
+  
 
-  constructor(private http: Http) {
-    super();
-    this.baseUrl = "http://localhost:4200/api/foods/"
+  constructor(http: Http) {
+    super(http);
   }
 
-  getAll(): Observable<Food[]> {
-    return null;
-  }
-
-  get(id: string): Observable<Food> {
-    return null;
-  }
-
-  update(food: Food) {
-    return null;
-  }
-
-  create(food: Food) {
-    return null;
+  baseUrl(): string {
+    return "http://localhost:4200/api/foods/";
   }
 }

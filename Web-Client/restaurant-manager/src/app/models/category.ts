e76384bb5cd65795b98e89c15c6e15eb89c398ce
Azizkhujaev, Jurabek <jurabek.azizkhujaev@gmail.com>
2017-08-03 16:32:41 +0300

    interface name changed

--- Web-Client/restaurant-manager/src/app/models/category.ts
+++ Web-Client/restaurant-manager/src/app/models/category.ts
@@ -1,6 +1,6 @@
-import { BaseModel } from "app/models/base.model";
+import { IBaseModel } from "app/models/base.model";
 
-export class Category implements BaseModel {
+export class Category implements IBaseModel {
     id: string;
     name: string;
     color: string;

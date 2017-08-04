fab45c74bd33d758784e9f0696dda876725e445a
Jurabek Azizkhujaev <jurabek.azizkhujaev@hotmail.com>
2017-08-04 00:26:41 -0700

    creating angular services

--- Web-Client/restaurant-manager/src/app/app.module.ts
+++ Web-Client/restaurant-manager/src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { FoodListComponent } from './components/foods/list.component';
 import { AddFoodComponent } from './components/foods/add.component';
 import { routes } from "app/app.router";
+import { AddCategoryComponent } from "app/components/categories/add.component";
+import { CategoryService } from "app/services/category.service";
+import { FoodService } from "app/services/food.service";
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     FoodListComponent,
-    AddFoodComponent
+    AddFoodComponent,
+    AddCategoryComponent
   ],
   imports: [
     routes,
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
   ],
-  providers: [],
+  providers: [FoodService, CategoryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Web-Client/restaurant-manager/src/app/app.router.ts
+++ Web-Client/restaurant-manager/src/app/app.router.ts
@@ -1,10 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { FoodListComponent } from "app/components/foods/list.component";
 import { AddFoodComponent } from "app/components/foods/add.component";
+import { AddCategoryComponent } from "app/components/categories/add.component";
 
 export const appRoutes: Routes = [
     { path: 'foods', component: FoodListComponent },
-    { path: 'foods/create', component: AddFoodComponent }
+    { path: 'foods/create', component: AddFoodComponent },
+    { path: 'categories', component: AddCategoryComponent }
 ];
 
 export const routes: any = RouterModule.forRoot(appRoutes);

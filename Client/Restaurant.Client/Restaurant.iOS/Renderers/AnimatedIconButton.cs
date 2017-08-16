a6f58569a7b71e1d4315efe12e3187aba712e9fd
Azizkhujaev, Jurabek <jurabek.azizkhujaev@gmail.com>
2017-08-16 11:50:47 +0300

    server dotnet cli command line feauture
    Former-commit-id: b2a49ee42e6cb8b95407ebf75c7189a76cb48788

---
+++ Client/Restaurant.Client/Restaurant.iOS/Renderers/AnimatedIconButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace Restaurant.iOS.Renderers
+{
+    public class AnimatedIconButton : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+            var a = ((UIButton) NativeView).ButtonType;
+        }
+    }
+}

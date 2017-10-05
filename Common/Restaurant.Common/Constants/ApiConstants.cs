0a6d9291e593cd00b039717474c1ae74c8c345f0
jurabek <jurabeka@pm.me>
2017-10-05 10:01:36 +0300

    small changes
    Former-commit-id: 3e735ea908839948b3c7775c12107ba6fa2c0d51

--- Common/Restaurant.Common/Constants/ApiConstants.cs
+++ Common/Restaurant.Common/Constants/ApiConstants.cs
@@ -3,8 +3,8 @@
     public static class ApiConstants
     {
 	    public const string ApiName = "api1";
-	    public const string ResourceClientId = "user_password.client";
-	    public const string MvcClientId = "mvc_id";
+	    public const string ClientId = "user_password.client";
+        public const string GrantType = "password";
 	    public const string ApiClientUrl = "http://localhost:62799/";
     }
 }

4944d96b6812090498faee2bb01604eced71836e
jurabek <jurabeka@pm.me>
2017-10-06 13:23:19 +0500

    ios UI changes
    Former-commit-id: 11fc147859a604dc2fcf9acbe2aa2a3447635104

--- Client/Restaurant.Client/Restaurant/ViewModels/FoodDetailViewModel.cs
+++ Client/Restaurant.Client/Restaurant/ViewModels/FoodDetailViewModel.cs
@@ -1,16 +1,24 @@
-﻿using Restaurant.Common.DataTransferObjects;
+﻿using ReactiveUI;
+using Restaurant.Common.DataTransferObjects;
 
 namespace Restaurant.ViewModels
 {
-    public class FoodDetailViewModel : ViewModelBase
-    {
-        public FoodDto SelectedFood { get; }
+	public class FoodDetailViewModel : ViewModelBase
+	{
+		public FoodDto SelectedFood { get; }
 
-        public FoodDetailViewModel(FoodDto selectedFood)
-        {
-            SelectedFood = selectedFood;
-        }
+		public FoodDetailViewModel(FoodDto selectedFood)
+		{
+			SelectedFood = selectedFood;
+		}
 
-        public override string Title => SelectedFood.Name;
-    }
+		public override string Title => SelectedFood.Name;
+
+		private double _quantity;
+		public double Quantity
+		{
+			get => _quantity;
+			set => this.RaiseAndSetIfChanged(ref _quantity, value);
+		}
+	}
 }

011e5b081673c18eb4b7e8d7b9f2567f3bd8d35e
Jurabek Azizkhujaev <jurabek.azizkhujaev@hotmail.com>
2017-10-15 23:34:03 +0500

    Tests
    Former-commit-id: 20c9a2bed6bd3645fa65e1446d5ea74f357f24d1

--- Server/Tests/Restaurant.Server.Api.UnitTests/Controllers/FoodsControllerTests.cs
+++ Server/Tests/Restaurant.Server.Api.UnitTests/Controllers/FoodsControllerTests.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
+using Moq;
 using Restaurant.Common.DataTransferObjects;
 using Restaurant.Server.Api.Abstractions.Facades;
 using Restaurant.Server.Api.Abstractions.Repositories;
@@ -20,14 +22,31 @@ namespace Restaurant.Server.Api.UnitTests.Controllers
 		    var foodDtos = Enumerable.Repeat(new FoodDto(), 5);
 
 			GetMock<IRepository<Food>>().Setup(x => x.GetAll()).Returns(foods.AsQueryable());
-		    GetMock<IMapperFacade>().Setup(x => x.Map<IEnumerable<FoodDto>>(foods.ToList())).Returns(foodDtos);
+		    GetMock<IMapperFacade>().Setup(x => x.Map<IEnumerable<FoodDto>>(It.IsAny<List<Food>>())).Returns(foodDtos);
 
 			// When
 		    var result = ClassUnderTest.Get();
 
 			// Then
-
 		    result.Should().Equal(foodDtos);
+	    }
+
+		[Fact]
+	    public void Given_food_id_Get_should_return_food()
+	    {
+			// Given
+		    var food = new Food();
+			var foodDto = new FoodDto();
+		    var id = Guid.NewGuid();
+
+		    GetMock<IRepository<Food>>().Setup(x => x.Get(id)).Returns(food);
+		    GetMock<IMapperFacade>().Setup(x => x.Map<FoodDto>(It.IsAny<Food>())).Returns(foodDto);
+
+			// When
+		    var result = ClassUnderTest.Get(id);
+
+		    // Then
+		    result.Should().Be(foodDto);
 	    }
     }
 }

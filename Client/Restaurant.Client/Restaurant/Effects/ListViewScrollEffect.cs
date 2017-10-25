b416794e061bd6c22e8ed2588d76c390674fcedf
Jurabek Azizkhujaev <jurabek.azizkhujaev@hotmail.com>
2017-10-25 14:27:35 +0500

    HIde android toolbar when scrolling
    Former-commit-id: fb4326683695620745714de92d0fe892656e5a61

---
+++ Client/Restaurant.Client/Restaurant/Effects/ListViewScrollEffect.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace Restaurant.Effects
+{
+	public class ListViewScrollEffect : RoutingEffect
+	{
+		public ListViewScrollEffect() : base("Restaurant.ListViewScrollEffect")
+		{
+		}
+	}
+}

7637d0c1d6f5dbfb73f967d282119580b9216c1a
jurabek <jurabeka@pm.me>
2017-10-25 14:27:35 +0500

    HIde android toolbar when scrolling
    Former-commit-id: fb4326683695620745714de92d0fe892656e5a61

--- Client/Restaurant.Client/Restaurant/Views/FoodsListView.xaml.cs
+++ Client/Restaurant.Client/Restaurant/Views/FoodsListView.xaml.cs
@@ -1,12 +1,15 @@
-﻿using Xamarin.Forms;
+﻿using Restaurant.Effects;
+using Xamarin.Forms;
 
 namespace Restaurant.Views
 {
+	// ReSharper disable once RedundantExtendsListEntry
     public partial class FoodsListView : ListView
     {
         public FoodsListView() : base(ListViewCachingStrategy.RecycleElement)
         {
             InitializeComponent();
         }
-    }
+
+	}
 }

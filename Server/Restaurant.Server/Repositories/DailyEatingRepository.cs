b2bf842113286cb5e2c1ed1877e368ba04e0d4f4
Jurabek Azizkhujaev <jurabek.azizkhujaev@hotmail.com>
2017-11-06 01:40:56 +0200

    Unit tests
    Former-commit-id: fde12770e6820a3ae34bc19c33f426753abaf1f9
    Former-commit-id: 3b5220cbe78b0b17cf089cf5da78d0a71fd3e864 [formerly 38c5ea42fca80780f49095ab7f44ac27b9da6dbe]
    Former-commit-id: 4904bceaf9012c331d96f35c8364068de5ad4dd1

--- Server/Restaurant.Server/Repositories/DailyEatingRepository.cs
+++ Server/Restaurant.Server/Repositories/DailyEatingRepository.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Logging;
 using Restaurant.Server.Api.Models;
 
 namespace Restaurant.Server.Api.Repositories
 {
+	[ExcludeFromCodeCoverage]
 	public class DailyEatingRepository : RepositoryBase<DailyEating>
 	{
 		public DailyEatingRepository(DatabaseContext context, ILogger<DailyEatingRepository> logger)

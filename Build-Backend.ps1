dfdff8ef7237a3fd7cf9f532aafe8846392e7c67
Jurabek Azizkhujaev <jurabek.azizkhujaev@hotmail.com>
2017-11-09 23:25:14 +0200

    Build script for back-end
    Former-commit-id: c0a13ebcba1a38d317c6a8c7f3bcfd40f4495722
    Former-commit-id: db0d5b01fb9afc4edb0e96e239c6700b9ae328e1 [formerly 5c875a86da54990851af903e9726aea45f275020]
    Former-commit-id: 743cf7ae19be0494b845443df5eb50481caa0f44

---
+++ Build-Backend.ps1
@@ -0,0 +1,35 @@
+param(
+    [string]$Config = "Release",  
+    [string]$Framework = "netcoreapp2.0",  
+    [string]$SolutionName = "Restaurant.Server.sln",
+    [string]$TestProjectFile = "Server/Tests/Restaurant.Server.Api.UnitTests/Restaurant.Server.Api.UnitTests.csproj",
+    [string]$OpenCover = "packages/OpenCover.4.6.519/tools/OpenCover.Console.exe",
+    [string]$CodeCov = "packages/Codecov.1.0.3/tools/codecov.exe",
+    [string]$CodeCovToken = "077aea61-7f42-49aa-b825-56c1e7c7095b"
+)
+
+$CoverageFolder = "coverage/"
+
+Write-Host "Installing OpenCover and Codecov via nuget"
+./.nuget/nuget.exe install -Verbosity quiet -OutputDirectory packages -Version 4.6.519 OpenCover
+./.nuget/nuget.exe install -Verbosity quiet -OutputDirectory packages -Version 1.0.3 Codecov
+
+Write-Host "Restoring $SolutionName"
+dotnet restore $SolutionName --no-cache
+
+Write-Host "Building $SolutionName"
+dotnet build $SolutionName -c $Config
+
+Write-Host "Testing $SolutionName"
+dotnet test -f $Framework -c $Config $TestProjectFile
+
+Write-Host "Removing coverage folder"
+Remove-Item -Recurse -Force $CoverageFolder
+New-Item -ItemType Directory -Path $CoverageFolder
+
+Write-Host "Calculating coverage with OpenCover"
+
+& $OpenCover -target:"c:\Program Files\dotnet\dotnet.exe" -targetargs:"test -f $Framework -c $Config $TestProjectFile" -mergeoutput -hideskipped:File -output:"$CoverageFolder/coverage.xml" -oldStyle -filter:"+[Restaurant.Server.Api*]* -[Restaurant.Server.Api.UnitTests*]*" -searchdirs:"Server/Tests/Restaurant.Server.Api.UnitTests/bin/$Config/$Framework" -register:user -excludebyattribute:*.ExcludeFromCodeCoverage*
+
+Write-Host "Uploading coverage results to CodeCov"
+& $CodeCov -f "$CoverageFolder/coverage.xml" -t $CodeCovToken

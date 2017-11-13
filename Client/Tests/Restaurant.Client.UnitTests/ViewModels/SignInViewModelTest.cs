2b605b8a4f7635eb692fbfcac7f47d9329f9fe27
Jurabek Azizkhujaev <jurabek.azizkhujaev@gmail.com>
2017-11-14 01:18:37 +0200

    Test project migrated from .NET Framework to .NET Core project

--- Client/Tests/Restaurant.Client.UnitTests/ViewModels/SignInViewModelTest.cs
+++ Client/Tests/Restaurant.Client.UnitTests/ViewModels/SignInViewModelTest.cs
@@ -9,7 +9,7 @@ using Restaurant.Abstractions.ViewModels;
 using Restaurant.Common.DataTransferObjects;
 using Restaurant.Core.ViewModels;
 
-namespace Restaurant.UnitTests.ViewModels
+namespace Restaurant.Client.UnitTests.ViewModels
 {
 	[TestFixture]
 	public class SignInViewModelTest : BaseAutoMockedTest<SignInViewModel>

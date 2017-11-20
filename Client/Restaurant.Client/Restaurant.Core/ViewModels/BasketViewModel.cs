fd020144c4a2d237da030e18fadb4465b9d3736c
Jurabek Azizkhujaev <jurabek.azizkhujaev@gmail.com>
2017-11-20 17:53:21 +0200

    added icons and Total Price for basket page
    Former-commit-id: 26f7a9ee283fef5eaf5e5a45edf02e140ac2b6c1
    Former-commit-id: 7dc18e1d456bec975558e8c73861a07382296ef5 [formerly a2703eab213043401b1922ef096aa956e1ff8ec6]
    Former-commit-id: 728b9e108242ee2f20a360b30c22c8c826df1b19

--- Client/Restaurant.Client/Restaurant.Core/ViewModels/BasketViewModel.cs
+++ Client/Restaurant.Client/Restaurant.Core/ViewModels/BasketViewModel.cs
@@ -1,51 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
+using System.Reactive.Linq;
 using System.Windows.Input;
 using ReactiveUI;
+using Restaurant.Abstractions.Adapters;
 using Restaurant.Abstractions.Api;
-using Restaurant.Abstractions.Facades;
 using Restaurant.Abstractions.Services;
 using Restaurant.Abstractions.ViewModels;
-using Restaurant.Common.DataTransferObjects;
 
 namespace Restaurant.Core.ViewModels
 {
     public class BasketViewModel : BaseViewModel, IBasketViewModel
     {
         private ReactiveList<IOrderViewModel> _orders = new ReactiveList<IOrderViewModel> { ChangeTrackingEnabled = true };
-        private string _ordersCount;
+        private readonly ObservableAsPropertyHelper<decimal> _totalPrice;
+        private readonly ObservableAsPropertyHelper<string> _ordersCount;
 
         public BasketViewModel(
             IOrdersApi ordersApi,
-            IAutoMapperFacade mapperFacade,
-            INavigationService navigationService)
+            INavigationService navigationService,
+            IOrderDtoAdapter orderDtoAdapter)
         {
-            this.WhenAnyValue(x => x.Orders.Count).Subscribe(x => { OrdersCount = x == 0 ? null : x.ToString(); });
-            
+           _ordersCount = this.WhenAnyValue(x => x.Orders.Count)
+                .Select(count => count == 0 ? null : count.ToString())
+                .ToProperty(this, x => x.OrdersCount);
+
+            _totalPrice = this.WhenAnyObservable(vm => vm.Orders.ItemChanged)
+                .Select(x => Orders.Sum(o => o.TotalPrice))
+                .ToProperty(this, x => x.TotalPrice);
+
             CompleteOrder = ReactiveCommand.Create(() =>
             {
-                var orderItems = mapperFacade.Map<IEnumerable<OrderItemDto>>(Orders);
-                ordersApi.Create(new OrderDto()
-                {
-                    DateTime = DateTime.Now,
-                    OrderItems = new List<OrderItemDto>(orderItems)
-                });
+                var orderDto = orderDtoAdapter.GetOrderDto(Orders);
+                ordersApi.Create(orderDto);
                 Orders.Clear();
                 navigationService.NavigateToRoot();
             });
         }
 
+        public decimal TotalPrice => _totalPrice.Value;
+
+        public string OrdersCount => _ordersCount.Value;
+
         public ReactiveList<IOrderViewModel> Orders
         {
             get => _orders;
             set => this.RaiseAndSetIfChanged(ref _orders, value);
-        }
-
-        public string OrdersCount
-        {
-            get => _ordersCount;
-            set => this.RaiseAndSetIfChanged(ref _ordersCount, value);
         }
 
         public void AddOrder(IOrderViewModel order)
@@ -56,7 +55,7 @@ namespace Restaurant.Core.ViewModels
                 .GroupBy(x => x.Food.Id)
                 .Select(orders => new OrderViewModel(orders.Select(x => x.Food).FirstOrDefault(x => x.Id == orders.Key), orders.Sum(s => s.Quantity)));
 
-            Orders = new ReactiveList<IOrderViewModel>(groupedOrders);
+            Orders = new ReactiveList<IOrderViewModel>(groupedOrders) { ChangeTrackingEnabled = true };
         }
 
         public ICommand CompleteOrder { get; }

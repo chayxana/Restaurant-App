75d0e2ebce0c6b3eb69a09b5105a0771ce7cdfb3
Jurabek Azizkhujaev <jurabek.azizkhujaev@gmail.com>
2017-11-20 15:16:12 +0200

    CI tests failing issue
    Former-commit-id: 3b4b8a62e317f02b14c1201bc62fe30252448494
    Former-commit-id: d3323605ba9493a22d3110cd52f87312416900a8 [formerly 719eb3ddfcca71f01fb859a0855c8a6883f2ef9d]
    Former-commit-id: 851f7bed117eae3f8fc339d7c595b0c3c33314ce

---
+++ Server/Restaurant.Server.Api/Controllers/OrdersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant.Server.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class OrdersController : Controller
+    {
+        // GET: api/Orders
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Orders
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Orders/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

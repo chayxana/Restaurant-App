8cda22d636f5ad6d4c36d3688bb92d3fff57fbb1
jurabek <jurabeka@pm.me>
2017-11-24 21:36:29 +0200

    removed angular project from this repository
    Former-commit-id: e2f9074104f30dc0efea1a08c859cfc7e5482bda
    Former-commit-id: 4c1f7a087c0f859ff0affcbe86679adcb58e10f6 [formerly 7d2303134de513d1088197dbcddd1b36a6d79276]
    Former-commit-id: c1acb0e68708b3e73b8984c1989752af49ecd015

--- Dashboard-Admin/src/app/models/base.model.ts
+++
@@ -1,3 +0,0 @@
-export interface IBaseModel {
-    id: string;
-}
--- Dashboard-Admin/src/app/models/category.ts
+++
@@ -1,7 +0,0 @@
-import { IBaseModel } from "app/models/base.model";
-
-export class Category implements IBaseModel {
-    id: string;
-    name: string;
-    color: string;
-}

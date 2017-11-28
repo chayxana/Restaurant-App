cb06249e76cb1c5a17e2c1cd7e8291f21ea1c3f1
jurabek <jurabeka@pm.me>
2017-11-28 23:14:52 +0200

    Trying to fix Identity Server issue with integration tests
    Former-commit-id: 81ac85024b646f0821526366a0367978d8d78f32
    Former-commit-id: f8bf2ddf078d96beb3c61e88aad36d6f9f47629d [formerly ce2f8cae5610b344a5cbf00fb2c78b14c42288e5]
    Former-commit-id: e5b942c28b8dbf8400eab8186f8966b1c4c9c736

---
+++ src/Server/Restaurant.Server.Api/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant.Server.Api.Controllers
+{
+	[Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+		[HttpGet]
+		[Authorize]
+	    public IEnumerable<string> Get()
+		{
+			return new[] {"Test1", "Test1", "Test1", "Test1", "Test1", "Test1", "Test1", "Test1"};
+		}
+    }
+}
---
+++ src/Server/Tests/Restaurant.Server.Api.IntegrationTests/Config/IdentityServerSetup.cs
@@ -0,0 +1,107 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurant.Common.Constants;
+using Restaurant.Server.Auth;
+
+namespace Restaurant.Server.Api.IntegrationTests.Config
+{
+    public class IdentityServerSetup
+    {
+        private readonly string _apiName;
+        private TestServer _identityServer;
+        private const string TokenEndpoint = "http://localhost/connect/token";
+        public HttpMessageHandler _handler;
+
+        public IdentityServerSetup()
+        {
+	        _apiName = ApiConstants.ApiName;
+        }
+
+        
+        public IdentityServerSetup Initialize()
+        {
+            InitializeIdentityServer();
+            return this;
+        }
+
+        public void  GetIdentityServerAuthenticationOptions(IdentityServerAuthenticationOptions options)
+        {
+	        
+        }
+
+        public async Task<string> GetAccessTokenForUser(string userName, string password, string clientId = ApiConstants.ClientId, string clientSecret = ApiConstants.ClientSecret)
+        {
+            var client = new TokenClient(TokenEndpoint, clientId, clientSecret, innerHttpMessageHandler: _handler);
+            
+            var response = await client.RequestResourceOwnerPasswordAsync(userName, password, _apiName);
+            return response.AccessToken;
+        }
+
+        public async Task SetAccessToken(HttpClient client, string username, string password, string clientId = ApiConstants.ClientId, string clientSecret = ApiConstants.ClientSecret)
+        {
+            var token = await GetAccessTokenForUser(username, password, clientId, clientSecret);
+            client.SetBearerToken(token);
+        }
+
+        private void InitializeIdentityServer()
+        {
+	        var projectDir = Directory.GetCurrentDirectory();
+	        var configuration = new ConfigurationBuilder()
+		        .SetBasePath(projectDir)
+		        .AddJsonFile("appsettings.json").Build();
+
+	        var builder = new WebHostBuilder()
+		        .UseEnvironment("Test")
+		        .UseContentRoot(projectDir)
+		        .UseConfiguration(configuration)
+		        .ConfigureServices(
+			        services =>
+			        {
+				        services.AddAuthentication(o =>
+				        {
+					        o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+					        o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				        }).AddJwtBearer(o =>
+				        {
+					        o.Authority = "http://localhost";
+					        o.Audience = ApiConstants.ApiName;
+					        o.RequireHttpsMetadata = false;
+				        });
+			        })
+				.UseStartup<Startup>();
+
+
+			_identityServer = new TestServer(builder);
+            _handler = _identityServer.CreateHandler();
+        }
+		
+
+        private void ConfigureIdentityServerApp(IApplicationBuilder app)
+        {
+            app.UseIdentityServer();
+            app.UseMvc();
+		}
+
+		private void ConfigureIdentityServerServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryPersistedGrants()
+                .AddInMemoryIdentityResources(Auth.Config.GetIdentityResources())
+                .AddInMemoryApiResources(Auth.Config.GetApiResources())
+                .AddInMemoryClients(Auth.Config.GetClients())
+                .AddTestUsers(DefaultUsers.Get());
+            services.AddMvc();
+        }
+		
+    }
+}

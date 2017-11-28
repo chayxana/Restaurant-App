5cb23469a5b82050fe70176ec370a45b8a7b5edd
jurabek <jurabeka@pm.me>
2017-11-28 18:24:49 +0200

    Integration tests
    Former-commit-id: 5bfe111c1e60f3b375b18628490d6ba2b656af89
    Former-commit-id: 83eb5394fb15cbded3ab67a714fe4e864394e6dd [formerly 7eaac44d226213b02fb7cc88117e6a78a91793da]
    Former-commit-id: dd2019fabe707e256fcc4218a3986672254ed5f0

---
+++ src/Server/Restaurant.Server.Auth/IdentityServerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Restaurant.Server.Auth
+{
+    public static class IdentityServerExtensions
+    {
+        public static IIdentityServerBuilder SetupIdentityServer(this IServiceCollection services, IHostingEnvironment env)
+        {
+            var identityServerBuilder = services.AddIdentityServer();
+            
+            if (env.IsEnvironment("Test"))
+                identityServerBuilder.AddTestUsers(DefaultUsers.Get());
+
+            return identityServerBuilder
+                .AddSigningCredential("CN=restaurantcert")
+                .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients());
+        }
+    }
+}
---
+++ src/Server/Tests/Restaurant.Server.Api.IntegrationTests/IntegrationTestBase.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Restaurant.Common.Constants;
+
+namespace Restaurant.Server.Api.IntegrationTests
+{
+    public class IntegrationTestBase : IDisposable
+    {
+        private readonly string _apiName = ApiConstants.ApiName;
+        private const string TokenEndpoint = "http://localhost/connect/token";
+        private readonly TestServer _testServer;
+        protected readonly HttpClient HttpClient;
+        private HttpMessageHandler _handler;
+
+        protected IntegrationTestBase()
+        {
+            var projectDir = Directory.GetCurrentDirectory();
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(projectDir)
+                .AddJsonFile("appsettings.json").Build();
+
+            var builder = new WebHostBuilder()
+                .UseEnvironment("Test")
+                .UseContentRoot(projectDir)
+                .UseConfiguration(configuration)
+                .UseStartup<Startup>();
+
+            _testServer = new TestServer(builder);
+            _handler = _testServer.CreateHandler();
+            HttpClient = _testServer.CreateClient();
+        }
+
+        protected Task SetUpTokenFor(string username, string password)
+        {
+            return SetAccessToken(HttpClient, username, password);
+        }
+
+   
+        private async Task<string> GetAccessTokenForUser(string userName, string password, string clientId = ApiConstants.ClientId, string clientSecret = ApiConstants.ClientSecret)
+        {
+            var handler = _testServer.CreateHandler();
+            var discoveryClient = new DiscoveryClient("http://localhost", handler);
+            var discoveryDocument = await discoveryClient.GetAsync();
+
+            var client = new TokenClient(discoveryDocument.TokenEndpoint, clientId, clientSecret, innerHttpMessageHandler : handler);
+
+            var response = await client.RequestResourceOwnerPasswordAsync(userName, password, _apiName);
+            return response.AccessToken;
+        }
+
+        private async Task SetAccessToken(HttpClient client, string username, string password, string clientId = ApiConstants.ClientId, string clientSecret = ApiConstants.ClientSecret)
+        {
+            var token = await GetAccessTokenForUser(username, password, clientId, clientSecret);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+
+        public virtual void Dispose()
+        {
+            _testServer.Dispose();
+            HttpClient?.Dispose();
+        }
+    }
+}

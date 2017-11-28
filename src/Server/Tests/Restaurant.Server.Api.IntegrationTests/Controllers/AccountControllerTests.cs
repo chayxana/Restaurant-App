0c4533a7e1a7771457c3bc3077146f2c3a7e0e8a
Azizkhujaev, Jurabek <jurabek.azizkhujaev@gmail.com>
2017-11-28 18:24:49 +0200

    Integration tests
    Former-commit-id: 5bfe111c1e60f3b375b18628490d6ba2b656af89
    Former-commit-id: 83eb5394fb15cbded3ab67a714fe4e864394e6dd [formerly 7eaac44d226213b02fb7cc88117e6a78a91793da]
    Former-commit-id: dd2019fabe707e256fcc4218a3986672254ed5f0

---
+++ src/Server/Tests/Restaurant.Server.Api.IntegrationTests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Restaurant.Server.Api.IntegrationTests.Controllers
+{
+    public class AccountControllerTests : IntegrationTestBase
+    {
+        //[Fact]
+        //public async Task GetUserInfo_should_return_unauthorized_for_unauthorized_user()
+        //{
+        //    // Act
+        //    var result = await HttpClient.GetAsync("/api/account/GetUserInfo");
+
+        //    // Assert
+        //    Assert.Equal(HttpStatusCode.Unauthorized, result.StatusCode);
+        //}
+
+        [Fact]
+        public async Task GetUserInfo_should_return_OK_for_authorized_user()
+        {
+            // Arrange
+            await SetUpTokenFor("admin", "Test@123");
+
+            // Act
+            var result = await HttpClient.GetAsync("/api/Account/GetUserInfo");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        }
+    }
+}

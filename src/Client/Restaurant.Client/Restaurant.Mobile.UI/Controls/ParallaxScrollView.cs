3e6f2fe97c52fae865d0483696f6503e64638286
Jurabek Azizkhujaev <jurabek.azizkhujaev@gmail.com>
2018-07-08 21:50:37 +0300

    nuget package updates

--- src/Client/Restaurant.Client/Restaurant.Mobile.UI/Controls/ParallaxScrollView.cs
+++ src/Client/Restaurant.Client/Restaurant.Mobile.UI/Controls/ParallaxScrollView.cs
@@ -2,52 +2,51 @@
 
 namespace Restaurant.Mobile.UI.Controls
 {
-    public class ParallaxScrollView : ScrollView
-    {
-        public static readonly BindableProperty ParallaxViewProperty =
-            BindableProperty.Create(nameof(ParallaxView), typeof(View), typeof(ParallaxScrollView), null);
+	public class ParallaxScrollView : ScrollView
+	{
+		public static readonly BindableProperty ParallaxViewProperty =
+			BindableProperty.Create(nameof(ParallaxView), typeof(View), typeof(ParallaxScrollView), null);
 
-        private double height;
+		private double height;
 
-        public ParallaxScrollView()
-        {
-            Scrolled += (sender, e) => Parallax();
-        }
+		public ParallaxScrollView()
+		{
+			Scrolled += (sender, e) => Parallax();
+		}
 
-        public View ParallaxView
-        {
-            get => (View) GetValue(ParallaxViewProperty);
-            set => SetValue(ParallaxViewProperty, value);
-        }
+		public View ParallaxView
+		{
+			get => (View)GetValue(ParallaxViewProperty);
+			set => SetValue(ParallaxViewProperty, value);
+		}
 
-        public void Parallax()
-        {
-            if (ParallaxView == null || Device.RuntimePlatform == Device.WinRT ||
-                Device.RuntimePlatform == Device.WinPhone)
-                return;
+		public void Parallax()
+		{
+			if (ParallaxView == null || Device.RuntimePlatform == Device.UWP)
+				return;
 
-            if (height <= 0)
-                height = ParallaxView.Height;
+			if (height <= 0)
+				height = ParallaxView.Height;
 
-            var y = -(int) ((float) ScrollY / 2.5f);
-            if (y < 0)
-            {
-                //Move the Image's Y coordinate a fraction of the ScrollView's Y position
-                ParallaxView.Scale = 1;
-                ParallaxView.TranslationY = y;
-            }
-            else if (Device.RuntimePlatform == Device.iOS)
-            {
-                //Calculate a scale that equalizes the height vs scroll
-                var newHeight = height + ScrollY * -1;
-                ParallaxView.Scale = newHeight / height;
-                ParallaxView.TranslationY = -(ScrollY / 2);
-            }
-            else
-            {
-                ParallaxView.Scale = 1;
-                ParallaxView.TranslationY = 0;
-            }
-        }
-    }
+			var y = -(int)((float)ScrollY / 2.5f);
+			if (y < 0)
+			{
+				//Move the Image's Y coordinate a fraction of the ScrollView's Y position
+				ParallaxView.Scale = 1;
+				ParallaxView.TranslationY = y;
+			}
+			else if (Device.RuntimePlatform == Device.iOS)
+			{
+				//Calculate a scale that equalizes the height vs scroll
+				var newHeight = height + ScrollY * -1;
+				ParallaxView.Scale = newHeight / height;
+				ParallaxView.TranslationY = -(ScrollY / 2);
+			}
+			else
+			{
+				ParallaxView.Scale = 1;
+				ParallaxView.TranslationY = 0;
+			}
+		}
+	}
 }

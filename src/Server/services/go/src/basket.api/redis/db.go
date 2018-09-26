d5a489ead1911a8c3ffc6a79456d9417a45c67ed
jurabek <jurabek.azizkhujaev@gmail.com>
2018-09-26 22:20:22 +0200

    folder structure changed for microservices including go, java, dotnet projects
    Former-commit-id: 22b66a163662596213b5d39d8a023cdbc6ac51dd
    Former-commit-id: 0736ac77a2e6506b11309e90d8bd1e5a03aa4759 [formerly 6f25413ecf974c9aaa262c55e2215f36d9631489]
    Former-commit-id: 53423cde6958ad79242a3f41ce7c74eebe021493

---
+++ src/Server/services/go/src/basket.api/redis/db.go
@@ -0,0 +1,61 @@
+package redis
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+// NewRedisPool creates new pool
+func NewRedisPool(server string) *redis.Pool {
+
+	return &redis.Pool{
+
+		MaxIdle:     3,
+		IdleTimeout: 240 * time.Second,
+
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", server)
+			if err != nil {
+				return nil, err
+			}
+			return c, err
+		},
+
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+
+// CleanupHook should be clean signals
+func CleanupHook(pool *redis.Pool) {
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	signal.Notify(c, syscall.SIGTERM)
+	signal.Notify(c, syscall.SIGKILL)
+	go func() {
+		<-c
+		pool.Close()
+		os.Exit(0)
+	}()
+}
+
+// HealthCheck checks redis server
+func HealthCheck(pool *redis.Pool) error {
+
+	conn := pool.Get()
+	defer conn.Close()
+
+	_, err := redis.String(conn.Do("PING"))
+	if err != nil {
+		return fmt.Errorf("cannot 'PING' db: %v", err)
+	}
+	return nil
+}

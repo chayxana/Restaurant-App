a91da510533174de304423a03c4003cedcc3e07d
jurabek <jurabek.azizkhujaev@gmail.com>
2018-10-30 21:35:17 +0100

    image name must be lower case issue fixed

--- src/Server/ci/build.sh
+++ src/Server/ci/build.sh
@@ -4,10 +4,10 @@ CI_API_NAME=$1
 
 main () {
     case "$CI_API_NAME" in
-        BASKET_API) build_basket_api ;;
-        ORDER_API) build_order_api ;;
-        IDENTITY_API) build_identity_api ;;
-        MENU_API) build_menu_api ;;
+        basket_api) build_basket_api ;;
+        order_api) build_order_api ;;
+        identity_api) build_identity_api ;;
+        menu_api) build_menu_api ;;
         all) 
             build_basket_api
             build_order_api
@@ -39,8 +39,9 @@ build_menu_api() {
 }
 
 docker_build_and_push() {
-    docker build --compress -t "$IMAGE_BASE_NAME:$CI_API_NAME" .
-    docker push "$IMAGE_BASE_NAME:$CI_API_NAME"
+    docker build --compress -t $IMAGE_BASE_NAME:$CI_API_NAME .
+    docker login
+    docker push $IMAGE_BASE_NAME:$CI_API_NAME
 }
 
 main "$@"

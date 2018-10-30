2bbb8c045895221e376ffb7e84e39ad80d612147
jurabek <jurabeka@pm.me>
2018-10-30 21:35:17 +0100

    image name must be lower case issue fixed
    Former-commit-id: 557d3e3fbca950c135c60f75040de7424be95cea
    Former-commit-id: 6894ff3ed34e00b55a709eeeef7caac11be62848 [formerly 420a43d453fd5b20564a89beae49513037027b1c]
    Former-commit-id: 0e2310c026aa22b65d2c9284630d2f0b7d3d7da9

--- src/Server/ci/test.sh
+++ src/Server/ci/test.sh
@@ -4,10 +4,10 @@ CI_API_NAME=$1
 
 main () {
     case "$CI_API_NAME" in
-        BASKET_API) test_basket_api ;;
-        ORDER_API) test_order_api ;;
-        IDENTITY_API) test_identity_api ;;
-        MENU_API) test_menu_api ;;
+        basket_api) test_basket_api ;;
+        order_api) test_order_api ;;
+        identity_api) test_identity_api ;;
+        menu_api) test_menu_api ;;
         all)
             build_basket_api
             build_order_api

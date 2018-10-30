452e22105aa5b78cc1ff13c68bfe2b709c08322b
jurabek <jurabeka@pm.me>
2018-10-30 20:30:20 +0100

    added test for basket_api and configured test stage in gitlab pipeline
    Former-commit-id: 1984a5afc5d50bb0f50ed7b0f0131ca4acc21908
    Former-commit-id: b56ef00eda7571308d199a2bdd1d74c3b9d55b7e [formerly d1f42bce4cc33c7e2d54a31d1fc02c488c8ffe19]
    Former-commit-id: b06a6d7247ef655772b5438131924f2054964db7

--- src/Server/ci/build.sh
+++ src/Server/ci/build.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-API_NAME=$1
+CI_API_NAME=$1
 
 main () {
-    ls
-    case "$API_NAME" in
-        basket) build_basket_api ;;
-        order) build_order_api ;;
-        identity) build_identity_api ;;
-        menu) build_menu_api ;;
+    case "$CI_API_NAME" in
+        BASKET_API) build_basket_api ;;
+        ORDER_API) build_order_api ;;
+        IDENTITY_API) build_identity_api ;;
+        MENU_API) build_menu_api ;;
         all) 
             build_basket_api
             build_order_api
@@ -22,21 +21,26 @@ main () {
 
 build_basket_api() {
     cd  ./services/go/src/basket.api/
-    docker build .
+    docker_build_and_push
 }
 
 build_order_api(){
     cd ./services/java/order.api/
-    docker build .
+    docker_build_and_push
 }
 build_identity_api() {
     cd ./services/netcore/Identity.API/
-    docker build .
+    docker_build_and_push
 }
 
 build_menu_api() {
     cd ./services/netcore/Menu.API/
-    docker build .
+    docker_build_and_push
+}
+
+docker_build_and_push() {
+    docker build --compress --force-rm -t "$IMAGE_BASE_NAME:$CI_API_NAME" .
+    docker push "$IMAGE_BASE_NAME:$CI_API_NAME"
 }
 
 main "$@"

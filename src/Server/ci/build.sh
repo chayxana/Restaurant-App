7a101dc77d100a95cac45df83a463bb8b3fdb258
jurabek <jurabek.azizkhujaev@gmail.com>
2018-10-30 21:37:17 +0100

    small fix

--- src/Server/ci/build.sh
+++ src/Server/ci/build.sh
@@ -40,7 +40,6 @@ build_menu_api() {
 
 docker_build_and_push() {
     docker build --compress -t $IMAGE_BASE_NAME:$CI_API_NAME .
-    docker login
     docker push $IMAGE_BASE_NAME:$CI_API_NAME
 }
 

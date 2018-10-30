7da63df60b3473c8a3a8d9744ba0b2eac12f0783
jurabek <jurabeka@pm.me>
2018-10-30 21:15:00 +0100

    build changes
    Former-commit-id: f2b593606e75eae0125f18e62de8c6ca1e7c0bb0
    Former-commit-id: 4e8a5c2ae9d731bd0bb2d38faa875b60a82054c6 [formerly 7a6dba8d323f07aaaa6574354ab58a5414011a2d]
    Former-commit-id: 1e67e8c1f8640943fe894e9fae664663a0192394

--- src/Server/ci/build.sh
+++ src/Server/ci/build.sh
@@ -39,7 +39,7 @@ build_menu_api() {
 }
 
 docker_build_and_push() {
-    docker build --compress --force-rm -t "$IMAGE_BASE_NAME:$CI_API_NAME" .
+    docker build --compress -t "$IMAGE_BASE_NAME:$CI_API_NAME" .
     docker push "$IMAGE_BASE_NAME:$CI_API_NAME"
 }
 

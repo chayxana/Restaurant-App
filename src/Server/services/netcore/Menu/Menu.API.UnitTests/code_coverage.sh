79efaedc11c8c3858834810e89c4f61cd6dd52bb
jurabek <jurabek.azizkhujaev@gmail.com>
2018-11-03 21:47:32 +0100

    DevOps stuff

---
+++ src/Server/services/netcore/Menu/Menu.API.UnitTests/code_coverage.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+generate_coverage(){
+    coverlet ./Menu.API.UnitTests.dll \
+    --target dotnet \
+    --targetargs "vstest ./Menu.API.UnitTests.dll" \
+    --format opencover \
+    --output "/app/coverage.xml"
+}
+
+generate_report() {
+    reportgenerator "-reports:/app/coverage.xml" "-targetdir:/app/coveragereport"
+}
+
+publish_repoport_to_github_pages(){
+    cd /app/coveragereport
+    git init
+    git config --global user.email "gitlabci@github.com"
+    git config --global user.name "Jurabek"
+    git remote add origin https://github.com/Jurabek/restaurant-menu-api-coverage.git
+    git pull
+    git add -A
+    git commit -m'Updating code coverage'
+    git push -f https://$GITHUB_USER_NAME:$GITHUB_USER_PASSWORD@github.com/Jurabek/restaurant-menu-api-coverage.git master
+}
+
+generate_coverage && generate_report && publish_repoport_to_github_pages

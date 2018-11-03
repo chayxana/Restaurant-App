86a59ae1e120c798c8a956aef209341cf1a15fd9
jurabek <jurabek.azizkhujaev@gmail.com>
2018-11-03 21:47:32 +0100

    DevOps stuff
    Former-commit-id: 0efe9b0f3bdb002219329dada313d3eebb004788
    Former-commit-id: 049df5b7275873f35aed5ad9217c6fe901c3c1a5 [formerly 8fef4e0f7e330b684ea9a44520135f1d8ffb4415]
    Former-commit-id: a6f001a27eb5ee74a8afb5dcbf81e5b264eade7f

--- src/Server/ci/test.sh
+++ src/Server/ci/test.sh
@@ -35,17 +35,16 @@ test_identity_api() {
 test_menu_api() {
     docker pull "$IMAGE_BASE_NAME:$CI_API_NAME"
     docker run --rm $IMAGE_BASE_NAME:$CI_API_NAME "dotnet" "vstest" "./Menu.API.UnitTests.dll"
-
-    docker run --name "$CI_API_NAME_coverage" $IMAGE_BASE_NAME:$CI_API_NAME /bin/bash -c \
-    "coverlet ./Menu.API.UnitTests.dll \
-    --target dotnet \
-    --targetargs \"vstest ./Menu.API.UnitTests.dll\" \
-    --format opencover \
-    --output /app/coverage.xml \
-    && reportgenerator \"-reports:/app/coverage.xml\" \"-targetdir:/app/coveragereport\" " 
-
-    docker cp $(docker ps -aqf "name=$CI_API_NAME_coverage"):/app/coveragereport ./
+    
+    # code coverage 
+    docker run -e GITHUB_USER_NAME="$GITHUB_USER_NAME" \
+    -e GITHUB_USER_PASSWORD=="$GITHUB_USER_PASSWORD" \
+    --name "$CI_API_NAME_coverage" \
+    $IMAGE_BASE_NAME:$CI_API_NAME /bin/bash -c ./code_coverage.sh | tee output.txt
     docker rm $(docker ps -aqf "name=$CI_API_NAME_coverage")
+    
+    COVERAGE_RESULT=$(grep "Total Branch" output.txt | tr -dc '[0-9]+\.[0-9]')
+    curl -o menu_coverage.svg https://img.shields.io/badge/coverage-$COVERAGE_RESULT%25-orange.svg
 }
 
 main "$@"

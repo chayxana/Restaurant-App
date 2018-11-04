e7c55ab6b89f4ef00074b90aca6aad613bbc7c39
jurabek <jurabek.azizkhujaev@gmail.com>
2018-11-04 16:34:01 +0100

    build badges
    Former-commit-id: 3605e8e28bbd097bba1bd21ff7c5af86279f31ea
    Former-commit-id: 4117c61522dff04c895917884098d21391e99ae7 [formerly 0b944ec7a0687a55903bbc7227aa492b3603ae20]
    Former-commit-id: 4b0253601e81e786e08a684945f2902e745e049a

---
+++ src/Server/ci/update_build_badge.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+STATUS=$1
+CI_API_NAME=$2
+
+# Set values for shields.io fields based on STATUS
+if [ $STATUS = "running" ]; then
+	BADGE_SUBJECT="build"
+	BADGE_STATUS="running"
+	BADGE_COLOR="lightgrey"
+elif [ $STATUS = "failure" ]; then
+	BADGE_SUBJECT="build"
+	BADGE_STATUS="failed"
+	BADGE_COLOR="red"
+elif [ $STATUS = "success" ]; then
+	BADGE_SUBJECT="build"
+	BADGE_STATUS="passed"
+	BADGE_COLOR="brightgreen"
+else
+	exit 1
+fi
+
+FILE_NAME="${CI_API_NAME}_build_status.svg"
+
+./ci/generate_badge.sh $FILE_NAME "$BADGE_SUBJECT" "$BADGE_STATUS" $BADGE_COLOR
+./ci/upload_badge.sh $FILE_NAME

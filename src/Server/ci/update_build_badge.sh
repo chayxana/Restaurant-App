0d5fae284b631c6b795549622fa2994af45ab5a8
jurabek <jurabek.azizkhujaev@gmail.com>
2018-11-19 23:48:52 +0100

    shields.io issue

--- src/Server/ci/update_build_badge.sh
+++ src/Server/ci/update_build_badge.sh
@@ -5,24 +5,24 @@ CI_API_NAME=$2
 API_TITLE=""
 
 case "$CI_API_NAME" in
-        basket_api) API_TITLE="basket-api" ;;
-        order_api) API_TITLE="order-api";;
-        identity_api) API_TITLE="identity-api" ;;
-        menu_api) API_TITLE="menu_api" ;;
+        basket_api) API_TITLE="basket--api" ;;
+        order_api) API_TITLE="order--api";;
+        identity_api) API_TITLE="identity--api" ;;
+        menu_api) API_TITLE="menu--api" ;;
         *) echo "Invalid api name!" ;;
     esac
 
 # Set values for shields.io fields based on STATUS
 if [ $STATUS = "running" ]; then
-	BADGE_SUBJECT="build-$API_TITLE"
+	BADGE_SUBJECT="build--$API_TITLE"
 	BADGE_STATUS="running"
 	BADGE_COLOR="yellow"
 elif [ $STATUS = "failure" ]; then
-	BADGE_SUBJECT="build-$API_TITLE"
+	BADGE_SUBJECT="build--$API_TITLE"
 	BADGE_STATUS="failed"
 	BADGE_COLOR="red"
 elif [ $STATUS = "success" ]; then
-	BADGE_SUBJECT="build-$API_TITLE"
+	BADGE_SUBJECT="build--$API_TITLE"
 	BADGE_STATUS="passed"
 	BADGE_COLOR="brightgreen"
 else

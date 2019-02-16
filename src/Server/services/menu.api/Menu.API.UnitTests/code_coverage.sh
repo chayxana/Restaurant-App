6b79bc5c75b04bdcf215c43ba345008bf52d166e
jurabek <jurabek.azizkhujaev@gmail.com>
2019-02-17 00:07:19 +0100

    DevOps changes
    Former-commit-id: 0ad0963cff4dba69f0c6e23a03b74f1206ebea70
    Former-commit-id: 0b20a0c297ac68a7169f34784a43e5c7a16af435 [formerly a271ee8b9c05fe08d1531df7ae7337d6d53cb005]
    Former-commit-id: d972530aa5db217066ac014c8dc77d7a7be720df

--- src/Server/services/menu.api/Menu.API.UnitTests/code_coverage.sh
+++ src/Server/services/menu.api/Menu.API.UnitTests/code_coverage.sh
@@ -12,18 +12,4 @@ generate_report() {
     reportgenerator "-reports:/app/coverage.xml" "-targetdir:/app/coveragereport"
 }
 
-publish_repoport_to_github_pages(){
-    cd /app/coveragereport
-    git init
-    git config --global user.email "gitlabci@github.com"
-    git config --global user.name "Gitlab_CI"
-    git remote add origin https://github.com/Jurabek/restaurant-menu-api-coverage.git
-    git pull
-    git add -A
-    DATE=`date +%Y-%m-%d`
-    COMMIT_MESSAGE="Updating code coverage-$DATE"
-    git commit -m"$COMMIT_MESSAGE"
-    git push -f https://$GITHUB_USER_NAME:$GITHUB_USER_PASSWORD@github.com/Jurabek/restaurant-menu-api-coverage.git master
-}
-
-generate_coverage && generate_report && publish_repoport_to_github_pages
+generate_coverage && generate_report

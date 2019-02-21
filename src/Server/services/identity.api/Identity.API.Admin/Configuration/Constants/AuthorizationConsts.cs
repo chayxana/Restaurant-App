ef9f93417a77ef78c7671660cf531bb3f2e524ea
jurabek <jurabek.azizkhujaev@gmail.com>
2019-02-21 17:08:45 +0100

    removed admin project

---
+++ src/Server/services/identity.api/Identity.API.Admin/Configuration/Constants/AuthorizationConsts.cs
@@ -0,0 +1,8 @@
+﻿namespace Identity.API.Admin.Configuration.Constants
+{
+    public class AuthorizationConsts
+    {
+        public const string AdministrationPolicy = "RequireAdministratorRole";
+        public const string AdministrationRole = "MyRole";
+    }
+}
---
+++ src/Server/services/identity.api/Identity.API.Admin/Configuration/Interfaces/IAdminConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace Identity.API.Admin.Configuration.Interfaces
+{
+    public interface IAdminConfiguration
+    {
+        string IdentityAdminRedirectUri { get; }
+
+        string IdentityServerBaseUrl { get; }
+
+        string IdentityAdminBaseUrl { get; }
+    }
+}

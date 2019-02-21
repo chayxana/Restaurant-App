9ea7a56641c4f11380e9d22346fdb2f07094a9ce
jurabek <jurabek.azizkhujaev@gmail.com>
2019-02-21 17:09:02 +0100

    removed identity admin project

--- src/Server/services/identity.api/Identity.API.Admin/Configuration/Constants/AuthorizationConsts.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Identity.API.Admin.Configuration.Constants
-{
-    public class AuthorizationConsts
-    {
-        public const string AdministrationPolicy = "RequireAdministratorRole";
-        public const string AdministrationRole = "MyRole";
-    }
-}

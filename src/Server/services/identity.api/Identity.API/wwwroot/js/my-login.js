9a18fc5e0323be43f9ed83c5e59b5d39017fb750
jurabek <jurabeka@pm.me>
2019-03-17 21:19:23 +0100

    service discovery pattern implemented on Basket API using golang
    Former-commit-id: 5718442f645e7b0c2f455258ec4995c82bbb1506
    Former-commit-id: 1350a918bb8ce6930f8ff09ee3119cc3b52634b1 [formerly 865b338424ee0c1e1d6ca4cbf63d7af2b5214c95]
    Former-commit-id: 0c083bfcc2dc7441a3a9f20d859f306a022f0e0a

---
+++ src/Server/services/identity.api/Identity.API/wwwroot/js/my-login.js
@@ -0,0 +1,84 @@
+/******************************************
+ * My Login
+ *
+ * Bootstrap 4 Login Page
+ *
+ * @author          Muhamad Nauval Azhar
+ * @uri 			https://nauval.in
+ * @copyright       Copyright (c) 2018 Muhamad Nauval Azhar
+ * @license         My Login is licensed under the MIT license.
+ * @github          https://github.com/nauvalazhar/my-login
+ * @version         1.2.0
+ *
+ * Help me to keep this project alive
+ * https://www.buymeacoffee.com/mhdnauvalazhar
+ * 
+ ******************************************/
+
+'use strict';
+
+$(function() {
+
+	$("input[type='password'][data-eye]").each(function(i) {
+		var $this = $(this),
+			id = 'eye-password-' + i,
+			el = $('#' + id);
+
+		$this.wrap($("<div/>", {
+			style: 'position:relative',
+			id: id
+		}));
+
+		$this.css({
+			paddingRight: 60
+		});
+		$this.after($("<div/>", {
+			html: 'Show',
+			class: 'btn btn-primary btn-sm',
+			id: 'passeye-toggle-'+i,
+		}).css({
+				position: 'absolute',
+				right: 10,
+				top: ($this.outerHeight() / 2) - 12,
+				padding: '2px 7px',
+				fontSize: 12,
+				cursor: 'pointer',
+		}));
+
+		$this.after($("<input/>", {
+			type: 'hidden',
+			id: 'passeye-' + i
+		}));
+
+		var invalid_feedback = $this.parent().parent().find('.invalid-feedback');
+
+		if(invalid_feedback.length) {
+			$this.after(invalid_feedback.clone());
+		}
+
+		$this.on("keyup paste", function() {
+			$("#passeye-"+i).val($(this).val());
+		});
+		$("#passeye-toggle-"+i).on("click", function() {
+			if($this.hasClass("show")) {
+				$this.attr('type', 'password');
+				$this.removeClass("show");
+				$(this).removeClass("btn-outline-primary");
+			}else{
+				$this.attr('type', 'text');
+				$this.val($("#passeye-"+i).val());				
+				$this.addClass("show");
+				$(this).addClass("btn-outline-primary");
+			}
+		});
+	});
+
+	$(".my-login-validation").submit(function() {
+		var form = $(this);
+        if (form[0].checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+		form.addClass('was-validated');
+	});
+});

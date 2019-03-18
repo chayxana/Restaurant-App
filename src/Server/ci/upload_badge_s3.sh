7d4bea7f5db0e64861fc9cb6b13104038f09bfe9
jurabek <jurabek.azizkhujaev@gmail.com>
2019-03-18 12:58:29 +0100

    gitlab-ci debugging
    Former-commit-id: 25316a169cc8f43f01e161fc1e5fe398654df947
    Former-commit-id: f71769ca3f6a91f3ddfca4349ffe6d3249c888ba [formerly 10f1b03a0232c671f72697958632a17f047e5ba1]
    Former-commit-id: a2ccb75ac5ea517a77dd5dab8a18607fd2cbb2e5

--- src/Server/ci/upload_badge_s3.sh
+++ src/Server/ci/upload_badge_s3.sh
@@ -6,4 +6,5 @@ aws configure set aws_access_key_id $AWS_KEY
 aws configure set aws_secret_access_key $AWS_SECRET
 aws configure set default.region eu-central-1
 
+aws s3 rm  s3://jurabek-restaurant-app/badges/$FILE_NAME
 aws s3 cp $FILE_NAME s3://jurabek-restaurant-app/badges/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers

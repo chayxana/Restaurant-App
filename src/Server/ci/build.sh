5c1eb487da06fb45a2e442f1ddcc9b830c418adf
jurabek <jurabek.azizkhujaev@gmail.com>
2019-03-18 22:50:37 +0100

    build script issue fixed

--- src/Server/ci/build.sh
+++ src/Server/ci/build.sh
@@ -55,6 +55,7 @@ build_menu_api() {
 }
 
 docker_build_and_push() {
+    echo "Docker build..."
     # docker build --compress -t $IMAGE_BASE_NAME:$CI_API_NAME .
     # docker push $IMAGE_BASE_NAME:$CI_API_NAME
 }

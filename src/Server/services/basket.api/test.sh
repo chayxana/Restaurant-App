eb49b5715e2cbf3a48b9dacb1ef266b14f1bb414
jurabek <jurabek.azizkhujaev@gmail.com>
2019-03-19 22:10:18 +0100

    basket-api-test-fails issue fixed
    Former-commit-id: c1670ade55dedb1ea005410ce4b6d0525a18ef11
    Former-commit-id: d951fbbc9251c6cf5f8297e696bd2578f78aa421 [formerly 5fa28464b273a3d3259937d9be48bce35bf7899b]
    Former-commit-id: 30620040a5bd162917e20054cd0d15eefcf931d8

--- src/Server/services/basket.api/test.sh
+++ src/Server/services/basket.api/test.sh
@@ -3,4 +3,4 @@
 apk add --no-cache git
 
 CGO_ENABLED=0 go test -v ./controllers --cover -coverprofile=coverage.out
-go tool cover -html=coverage.out -o coverage.html
+go tool cover -html=coverage.out -o ./reports/coverage.html

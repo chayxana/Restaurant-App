35a87fe343d8c03fc1df01bd3e3dcceac846fb24
jurabek <jurabek.azizkhujaev@gmail.com>
2019-03-19 21:52:01 +0100

    cache time issue
    Former-commit-id: 8f376c2564e74d8115d69b0636758799c251bb2e
    Former-commit-id: bb3f5081e8376da24f75bed661d5d2d512e23db8 [formerly d72ab07f37a1bfc25ceee3f96f85abcd402db069]
    Former-commit-id: f788dde1feefb40262426d9a53b6d0ae8b19f7f2

--- src/Server/services/basket.api/test.sh
+++ src/Server/services/basket.api/test.sh
@@ -2,5 +2,5 @@
 
 apk add --no-cache git
 
-go test -v ./controllers --cover -coverprofile=coverage.out
+CGO_ENABLED=0 go test -v ./controllers --cover -coverprofile=coverage.out
 go tool cover -html=coverage.out -o coverage.html

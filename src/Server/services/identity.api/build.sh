d81b20f2a2957516a536b0222db4db63e5a2e1a1
jurabek <jurabek.azizkhujaev@gmail.com>
2019-03-23 00:57:17 +0100

    identity api build
    Former-commit-id: a06b02e9113539d877ff47e543883ec35a30a24b
    Former-commit-id: 2f577cb0c9fa2c7277fd89344861e1bc840e28ee [formerly a5483762ddc17528de0fcd9410596665ee83c032]
    Former-commit-id: 8af73a8d1ebb816ab6cd6847a27004effa98f989

---
+++ src/Server/services/identity.api/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Restoring Identity.API"
+dotnet restore Identity.API/Identity.API.csproj
+
+echo "Building Identity.API"
+dotnet build Identity.API/Identity.API.csproj
+
+echo "Publishing Identity.API"
+dotnet publish Identity.API/Identity.API.csproj -c Release -o ./release
+
+echo "Moving output to root folder"
+mv ./Identity.API/release .
---
+++ src/Server/services/identity.api/test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Restoring Identity.API.UnitTests"
+dotnet restore Identity.API.UnitTests/Identity.API.UnitTests.csproj
+
+echo "Building Identity.API.UnitTests"
+dotnet build Menu.API.UnitTests/Identity.API.UnitTests.csproj -c Release -o ./output
+
+echo "Testing Identity.API.UnitTests"
+dotnet test Identity.API.UnitTests/Identity.API.UnitTests.csproj
+
+echo "Code coverage Identity.API"
+sh ./coverage.sh

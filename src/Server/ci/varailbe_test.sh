5825c16c0558c6da53c449101027fd07c18bad87
jurabek <jurabeka@pm.me>
2019-03-22 21:57:46 +0100

    DevOps changes menu_api tests and builds
    Former-commit-id: e9d44231575d3b9eef511829a410eda5ae74f457
    Former-commit-id: f91ecf812285a7b41270181df846afbbfa99deb3 [formerly 3b8ca72f0d98e57e570d31789171d02529f5512a]
    Former-commit-id: f927e605831a8ceaa508a9384197fd4d402c6b8a

--- src/Server/ci/varailbe_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export A=1
-
-echo $A
-
-export A=3
-
-echo $A
---
+++ src/Server/services/menu.api/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Restoring Menu.API"
+dotnet restore Menu.API/Menu.API.csproj
+
+echo "Building Menu.API"
+dotnet build Menu.API/Menu.API.csproj
+
+echo "Publishing Menu.API"
+dotnet publish Menu.API/Menu.API.csproj -c Release -o ./release
+
+echo "Moving output to root folder"
+mv ./Menu.API/release .

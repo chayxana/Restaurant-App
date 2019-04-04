611f9f74834bff97c4058899bb9e2a742ee9f961
jurabek <jurabeka@pm.me>
2019-04-05 00:49:03 +0200

    DevOps scripts: basket_api code coverage result fixed
    Former-commit-id: c1ef7b903b57a5ae4f0c2d80953185d4d6403ac1
    Former-commit-id: 7ed63bb807570ec736ab66861e7af31691717967 [formerly beadb71d46594278ba10862beb54d0a9417e7352]
    Former-commit-id: 81130cf7a80474fe96aff1b596475caf0387faf6

--- src/Server/services/basket.api/coverage_result.sh
+++ src/Server/services/basket.api/coverage_result.sh
@@ -1,12 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-covered=0
-total=0
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    IFS=' ' read -r -a array <<< "$line"
-    total=$(($total+${array[1]}))
-    if [ "${array[2]}" = "1" ]; then
-        covered=$(($covered+${array[1]}))
-    fi 
-done < "$1"
-echo $(awk "BEGIN { pc=100*${covered}/${total}; i=int(pc); print (pc-i<0.5)?i:i+1 }")
+OUTPUT_FILE=$1
+
+TOTAL_COVERAGE=$(awk '{if ($1 != "?") print $5; else print "0.0";}' $OUTPUT_FILE | sed 's/\%//g' | awk '{s+=$1} END {printf "%.2f\n", s}') 
+TOTAL_LINES=$(awk 'END{ print NR }' $OUTPUT_FILE)
+
+echo $(expr ${TOTAL_COVERAGE%.*} / $TOTAL_LINES)

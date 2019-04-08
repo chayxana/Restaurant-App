3be59773ca8aea3d196f1e5c4aee46e0ec6dc757
jurabek <jurabeka@pm.me>
2019-04-08 20:13:13 +0200

    go-jwt implementation
    Former-commit-id: aabf44cb881c3e4fb2378416eb819fa4bd42191d
    Former-commit-id: f35494527f8f6361399c9696942175944ef154d9 [formerly ca260d2381f5d45bfee6825740a8fe8faa89d82c]
    Former-commit-id: 4a84024bcf0c7115204032e9b892cba61f80b1ac

--- src/Server/services/basket.api/go.mod
+++ src/Server/services/basket.api/go.mod
@@ -9,8 +9,11 @@ require (
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/google/uuid v1.1.1
 	github.com/json-iterator/go v1.1.5 // indirect
+	github.com/lestrrat-go/jwx v0.0.0-20190407121834-9871a5674c2c // indirect
+	github.com/lestrrat-go/pdebug v0.0.0-20180220043849-39f9a71bcabe // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/okta/okta-jwt-verifier-golang v0.0.0-20180501213203-04702def3e1b
 	github.com/ory/go-acc v0.0.0-20181118080137-ddc355013f90 // indirect
 	github.com/ory/x v0.0.37 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
--- src/Server/services/basket.api/middlewares/auth.go
+++ src/Server/services/basket.api/middlewares/auth.go
@@ -3,24 +3,56 @@ package middlewares
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
-	jwt "github.com/dgrijalva/jwt-go"
-	request "github.com/dgrijalva/jwt-go/request"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/jurabek/basket.api/models"
 )
 
-func RequireTokenAuthentication(rw http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		r := c.Request
 
-	token, err := request.ParseFromRequest(req, request.OAuth2Extractor, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		} else {
-			return nil, nil
+		tokenHeader := r.Header.Get("Authorization") //Grab the token from the header
+
+		if tokenHeader == "" { //Token is missing, returns with error code 403 Unauthorized
+			httpError := models.NewHTTPError(http.StatusUnauthorized, fmt.Errorf("token is missing"))
+			c.AbortWithStatusJSON(http.StatusUnauthorized, httpError)
 		}
-	})
 
-	if err == nil && token.Valid {
-		next(rw, req)
-	} else {
-		rw.WriteHeader(http.StatusUnauthorized)
+		splitted := strings.Split(tokenHeader, " ") //The token normally comes in format `Bearer {token-body}`, we check if the retrieved token matched this requirement
+		if len(splitted) != 2 {
+			httpError := models.NewHTTPError(http.StatusUnauthorized, fmt.Errorf("bearer token invalid format"))
+			c.AbortWithStatusJSON(http.StatusUnauthorized, httpError)
+		}
+
+		bearerToken := splitted[1]
+
+		toValidate := map[string]string{}
+		toValidate["aud"] = "api://default"
+		toValidate["cid"] = "{CLIENT_ID}"
+
+		token, err := jwt.Parse(bearerToken, func(token *jwt.Token) (interface{}, error) {
+			// Don't forget to validate the alg is what you expect:
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+
+			// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+			return []byte("key"), nil
+		})
+
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			fmt.Println(claims["foo"], claims["nbf"])
+		} else {
+			fmt.Println(err)
+		}
+
+		if err != nil {
+			httpError := models.NewHTTPError(http.StatusUnauthorized, err)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, httpError)
+		}
+		c.Next()
 	}
 }

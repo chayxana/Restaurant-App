ca3f997cdeb34f713bac287694ed4ccbfeca96f1
jurabek <jurabeka@pm.me>
2019-08-08 23:17:57 +0200

    kubernetes support
    Former-commit-id: 38212429581d233f3694fcf2b2ce254a935a3dfe
    Former-commit-id: 40369ee10aa131f87e306bfda602d6a50b2cc98c [formerly 52ed479c72b56bc9ad6f01bd05a6c610ee1fb4be]
    Former-commit-id: a2209e0d65c58fdf975eb758aa2a4e1065ab2c5a

---
+++ src/Server/web/web.admin/dashboard/src/app/components/components.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
+
+@NgModule({
+  imports: [
+    ConfirmationDialogComponent
+  ],
+  declarations: []
+})
+export class ComponentModule {
+}
---
+++ src/Server/web/web.admin/dashboard/src/app/components/components.router.ts
@@ -0,0 +1,12 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ComRouter {
+}

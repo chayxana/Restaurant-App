920b1e7b36e8d6bd71040ffd022c3796dc7f76af
Jurabek <jurabek.azizkhujaev@gmail.com>
2019-08-20 22:45:30 +0200

    Gitlab mono repo features running pipelines when needed
    Former-commit-id: 42d3a36eb06d835018097d319fc2218ffdc162c5
    Former-commit-id: 06bec4e6611b12a658cb44639a533cad6ca0746b [formerly b9f31e1bdee29c14fe4e8e8bf6eee891cfc79a4d]
    Former-commit-id: 44eb345d1e4af6597ee0fb4929c7f307011db273

--- src/Server/services/menu.api/Menu.API/WebHostExtensions.cs
+++ src/Server/services/menu.api/Menu.API/WebHostExtensions.cs
@@ -2,6 +2,7 @@
 using System.Data.SqlClient;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Polly;
@@ -10,7 +11,7 @@ namespace Menu.API
 {
     public static class IWebHostExtensions
     {
-        public static IWebHost MigrateDbContext<TContext>(this IWebHost webHost, Action<TContext,IServiceProvider> seeder) where TContext : DbContext
+        public static IWebHost MigrateDbContext<TContext>(this IWebHost webHost, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
         {
             using (var scope = webHost.Services.CreateScope())
             {
@@ -19,40 +20,55 @@ namespace Menu.API
                 var logger = services.GetRequiredService<ILogger<TContext>>();
 
                 var context = services.GetService<TContext>();
+                var configuration = services.GetService<IConfiguration>();
+                var orchestrationType = configuration.GetValue<string>("OrchestrationType");
+                var k8s = orchestrationType.ToUpper().Equals("k8s");
 
                 try
                 {
                     logger.LogInformation($"Migrating database associated with context {typeof(TContext).Name}");
 
-                    var retry = Policy.Handle<SqlException>()
-                         .WaitAndRetry(new TimeSpan[]
-                         {
-                             TimeSpan.FromSeconds(5),
-                             TimeSpan.FromSeconds(10),
-                             TimeSpan.FromSeconds(15),
-                         });
-
-                    retry.Execute(() =>
+                    if (k8s)
                     {
-                        //if the sql server container is not created on run docker compose this
-                        //migration can't fail for network related exception. The retry options for DbContext only 
-                        //apply to transient exceptions.
+                        Migrate(seeder, services, context);
+                    }
+                    else
+                    {
+                        var retry = Policy.Handle<SqlException>()
+                             .WaitAndRetry(new TimeSpan[]
+                             {
+                                TimeSpan.FromSeconds(5),
+                                TimeSpan.FromSeconds(10),
+                                TimeSpan.FromSeconds(15),
+                             });
 
-                        context.Database.Migrate();
-
-                        seeder(context, services);
-                    });
-                  
-
+                        retry.Execute(() =>
+                        {
+                            //if the sql server container is not created on run docker compose this
+                            //migration can't fail for network related exception. The retry options for DbContext only 
+                            //apply to transient exceptions.
+                            Migrate(seeder, services, context);
+                        });
+                    }
                     logger.LogInformation($"Migrated database associated with context {typeof(TContext).Name}");
                 }
                 catch (Exception ex)
                 {
+                    if (k8s)
+                    {
+                        throw ex;
+                    }
                     logger.LogError(ex, $"An error occurred while migrating the database used on context {typeof(TContext).Name}");
                 }
             }
 
             return webHost;
+        }
+
+        private static void Migrate<TContext>(Action<TContext, IServiceProvider> seeder, IServiceProvider services, TContext context) where TContext : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, services);
         }
     }
 }

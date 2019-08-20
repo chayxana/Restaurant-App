8361108f74a8d026bb18ed3fc0bceebda6927e1f
Jurabek <jurabek.azizkhujaev@gmail.com>
2019-08-20 23:20:12 +0200

    Gitlab mono repo features running pipelines when needed
    Former-commit-id: 2908a12ba4f7876336ea1d3b6878e4bae8aff8d6
    Former-commit-id: 0ba81451135ad0567ecd51f9771f0d2155a9c529 [formerly 1b8076c45b108ef5218292160703e0dfa64d753a]
    Former-commit-id: f3b39375213530cbc983fa252d3bcf081ef0e9d8

--- src/Server/ci/upload_badge_s3.sh
+++ src/Server/ci/upload_badge_s3.sh
@@ -2,10 +2,10 @@
 
 FILE_NAME=$1
 
-aws configure set aws_access_key_id $AWS_KEY
-aws configure set aws_secret_access_key $AWS_SECRET
-aws configure set default.region eu-central-1
+su aws configure set aws_access_key_id $AWS_KEY
+su aws configure set aws_secret_access_key $AWS_SECRET
+su aws configure set default.region eu-central-1
 
-aws s3 cp $FILE_NAME s3://jurabek-restaurant-app/badges/ \
+su aws s3 cp $FILE_NAME s3://jurabek-restaurant-app/badges/ \
     --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers \
     --cache-control no-store

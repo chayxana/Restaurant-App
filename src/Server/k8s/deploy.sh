e5f30fbe386ebe8aa745bfbee6a4fbc99b42de94
jurabek <jurabeka@pm.me>
2019-08-20 17:37:48 +0200

    Ingress deployment
    Former-commit-id: a55d0da2a359afaad6f4c7e8250a8353fa2ef071
    Former-commit-id: 7285c56bca3675f3456f28f8ded38a974099477e [formerly 00aac5694809e2e2bcfde15b7330c942f306245b]
    Former-commit-id: a2264d5c54c88ad56064a06959cc78a11afcdbb4

--- src/Server/k8s/deploy.sh
+++ src/Server/k8s/deploy.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
-kubectl delete -f ingress.yml \
-               -f services.yml \
-               -f deployement.yml \
-               -f pgsql-data.yml \
-               -f redis-data.yml \
-               -f config-map.yml
+echo "################### Deleting k8s resources ###############"
 
-kubectl apply -f ingress.yml \
-               -f services.yml \
-               -f deployement.yml \
-               -f pgsql-data.yml \
-               -f redis-data.yml \
-               -f config-map.yml
+kubectl delete deployments --all
+kubectl delete services --all
+kubectl delete configmap local-config|| true
+kubectl delete -f ingress.yml
+
+echo "#################### Deploying infrastructure components ####################"
+kubectl create -f pgsql-data.yml -f redis-data.yml -f config-map.yml
+
+echo "#################### Creating services ####################"
+kubectl create -f services.yml
+kubectl create -f ingress.yml
+
+ip_regex='([0-9]{1,3}\.){3}[0-9]{1,3}|[A-B]|[a-b]'
+while true; do
+    printf "."
+    ingressUrl=$(kubectl get svc ingress-nginx -n ingress-nginx -o=jsonpath="{.status.loadBalancer.ingress[0].ip}" | kubectl get svc ingress-nginx -n ingress-nginx -o=jsonpath="{.status.loadBalancer.ingress[0].hostname}")
+    if [[ $ingressUrl =~ $ip_regex ]]; then
+        break
+    fi
+    sleep 5s
+done
+
+printf "\n"
+externalURL=$ingressUrl
+echo "Using $externalURL as the external DNS/IP of the K8s cluster"
+
+
+kubectl create -f deployment.yml
+
+echo "Basket API is exposed at http://$externalURL/basket/swagger/index.html
+Order API at http://$externalURL/order/swagger-ui.html 
+Menu at http://$externalURL/menu/swagger/index.html
+Identity http://$externalURL/identity/swagger/index.html"
+
+echo "Deployment DONE!"

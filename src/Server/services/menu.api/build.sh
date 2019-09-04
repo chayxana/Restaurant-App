6471d0d2db9012927addd37ce6c25ba24120f4c0
Jurabek <jurabek.azizkhujaev@gmail.com>
2019-09-04 21:00:51 +0200

    CI build problems
    Former-commit-id: d18316df080dc3f89b4c78c35f3b4d37f11d7926
    Former-commit-id: 97752c802302300ffd1c33b70baa121513b48420 [formerly 0c147821df19bbd9f6bcae18b5189c9adab646f5]
    Former-commit-id: 567d9e36a6307ea0218c3668f7e6520bd8214a22

--- src/Server/services/menu.api/build.sh
+++ src/Server/services/menu.api/build.sh
@@ -7,7 +7,7 @@ echo "Building Menu.API"
 dotnet build Menu.API/Menu.API.csproj
 
 echo "Publishing Menu.API"
-dotnet publish Menu.API/Menu.API.csproj -c Release -o ./release
+dotnet publish Menu.API/Menu.API.csproj -c Release -o ./menu-api-release
 
 echo "Moving output to root folder"
-mv ./Menu.API/release .
+mv ./Menu.API/menu-api-release .

21e354f191c28f9aabccfb9a0a9a8d5ffca685f6
Jurabek <jurabek.azizkhujaev@gmail.com>
2019-09-04 14:36:24 +0200

    Go proxy configured

--- src/Server/services/basket.api/build.sh
+++ src/Server/services/basket.api/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-apk add --no-cache git
+export GOPROXY=https://proxy.golang.org && export GOSUMDB=sum.golang.org && export GO111MODULE=on
 CGO_ENABLED=0 go build -installsuffix 'static'

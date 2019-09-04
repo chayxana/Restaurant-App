85e89aa3bbfabb261a0e5d38081f288fd5380436
Jurabek <jurabek.azizkhujaev@gmail.com>
2019-09-04 21:32:35 +0200

    gitlab pipeline improvements

--- src/Server/services/menu.api/build.sh
+++ src/Server/services/menu.api/build.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-echo "Restoring Menu.API"
-dotnet restore Menu.API/Menu.API.csproj
-
-echo "Building Menu.API"
-dotnet build Menu.API/Menu.API.csproj
-
 echo "Publishing Menu.API"
 dotnet publish Menu.API/Menu.API.csproj -c Release -o ./menu-api-release
 

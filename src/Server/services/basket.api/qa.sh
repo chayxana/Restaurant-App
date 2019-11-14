142bac51a835780d2d06723865e5cf8df6f36c10
jurabek <jurabeka@pm.me>
2019-11-14 23:34:22 +0100

    Eureka removed from build scripts
    Former-commit-id: 7d4e336db5999da9c4542519e76947f7800b5a63
    Former-commit-id: 03ab9edbfcbc33a7ffb04b2536b285207a31fc45 [formerly a4dcfa22f7c26fb323eab5732daaedaa155e34c4]
    Former-commit-id: fcb98696366cff8812484c4214868eaafc45c952

--- src/Server/services/basket.api/qa.sh
+++ src/Server/services/basket.api/qa.sh
@@ -1,3 +1,3 @@
-CGO_ENABLED=0 go test ./controllers ./repositories ./eureka ./oidc --cover -coverprofile=coverage.out
+CGO_ENABLED=0 go test ./controllers ./repositories ./oidc --cover -coverprofile=coverage.out
 
 sonar-scanner -Dsonar.login=423f19c6b4a3ad087de2e7039311d3f047bcb90e

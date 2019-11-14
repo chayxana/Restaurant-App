c441af9f36123a60287b76fe12fa5e555cc8f829
Jurabek <jurabek.azizkhujaev@gmail.com>
2019-11-14 16:25:31 +0100

    Sonarqube for PR

--- src/Server/services/basket.api/qa.sh
+++ src/Server/services/basket.api/qa.sh
@@ -1,3 +1,5 @@
 CGO_ENABLED=0 go test ./controllers ./repositories ./eureka ./oidc --cover -coverprofile=coverage.out
 
-sonar-scanner -Dsonar.login=423f19c6b4a3ad087de2e7039311d3f047bcb90e
+sonar-scanner -Dsonar.login=423f19c6b4a3ad087de2e7039311d3f047bcb90e \
+    -Dsonar.pullrequest.branch=$CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_NAME \
+    -Dsonar.pullrequest.key=$CI_EXTERNAL_PULL_REQUEST_IID
--- src/Server/services/order.api/qa.sh
+++ src/Server/services/order.api/qa.sh
@@ -4,4 +4,6 @@
         -Dsonar.projectKey=restaurant-order-api \
         -Dsonar.organization=restaurant-app \
         -Dsonar.host.url=https://sonarcloud.io \
-        -Dsonar.login=9c4bb9969c0a77dbb45d725b8979a49d0ecb28d7
+        -Dsonar.login=9c4bb9969c0a77dbb45d725b8979a49d0ecb28d7 \
+        -Dsonar.pullrequest.branch=$CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_NAME \
+        -Dsonar.pullrequest.key=$CI_EXTERNAL_PULL_REQUEST_IID

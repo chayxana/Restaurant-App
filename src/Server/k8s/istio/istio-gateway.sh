8e968e8630d49e9608ae3c3438c59e9b1a2a3d6f
Jurabek <jurabek.azizkhujaev@gmail.com>
2019-11-14 11:47:24 +0100

    Spring Boot 2.2 upgrade Swagger Auth URL override Istio Ingress Gateway added with VirtualServices Docker OpenJDK using OpenJ9 to improving perforamance in the cloud
    Former-commit-id: 67a561853bfea98fb6dbe0ab05e6002bd5f1d2f9
    Former-commit-id: 74afc28e8b615de17cc4b7a06d1583f1629f864d [formerly be322d03af7cf585f1f9bbdffeb1c45559c64133]
    Former-commit-id: f66914675723c8de22a460751e343cdd9f006113

---
+++ src/Server/k8s/istio/istio-gateway.sh
@@ -0,0 +1,3 @@
+export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
+export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
--- src/Server/services/order.api/src/main/java/com/jurabek/restaurant/order/api/services/CustomerBasketServiceIml.java
+++ src/Server/services/order.api/src/main/java/com/jurabek/restaurant/order/api/services/CustomerBasketServiceIml.java
@@ -1,6 +1,7 @@
 package com.jurabek.restaurant.order.api.services;
 
 public class CustomerBasketServiceIml implements CustomerBasketService {
+    
     @Override
     public void clearCustomerBasket(String customerId) {
 

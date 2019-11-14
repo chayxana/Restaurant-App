6df04ae8a3b84f97ea380132aa9ec4da82574e6c
Jurabek <jurabek.azizkhujaev@gmail.com>
2019-11-14 23:23:29 +0100

    Sonarqube Github PR update with Sonar Github App
    Former-commit-id: 7b9cb48bae5b12338756fee1dd22842948b45d38
    Former-commit-id: bfdd53d5ac1e9338e3502f0149be7dd1240f9bdb [formerly ea2524913349c75193ec903d8c3a5c4745a7ff06]
    Former-commit-id: ded572cbc7981d45eed48e1876f1c41437b4aa7d

--- src/Server/services/order.api/qa.sh
+++ src/Server/services/order.api/qa.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
  
  ./gradlew sonarqube \
-        -Dsonar.projectKey=restaurant-order-api \
-        -Dsonar.organization=restaurant-app \
-        -Dsonar.host.url=https://sonarcloud.io \
-        -Dsonar.login=9c4bb9969c0a77dbb45d725b8979a49d0ecb28d7 \
-        -Dsonar.pullRequest=$CI_EXTERNAL_PULL_REQUEST_IID \
-        -Dsonar.github.repository=Jurabek/Restaurant-App \
-        -Dsonar.github.oauth=$GITHUB_SONAR_KEY \
-        -Dsonar.pullrequest.github.endpoint=https://github.com/api/v3
+       -Dsonar.projectKey=restaurant-order-api \
+       -Dsonar.organization=restaurant-app \
+       -Dsonar.host.url=https://sonarcloud.io \
+       -Dsonar.login=9c4bb9969c0a77dbb45d725b8979a49d0ecb28d7 \
+       -Dsonar.github.repository=Jurabek/Restaurant-App \
+       -Dsonar.pullrequest.base=$CI_EXTERNAL_PULL_REQUEST_TARGET_BRANCH_NAME \
+       -Dsonar.pullrequest.branch=$CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_NAME \
+       -Dsonar.pullrequest.key=$CI_EXTERNAL_PULL_REQUEST_IID \
+       -Dsonar.pullrequest.provider=GitHub

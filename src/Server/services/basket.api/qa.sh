9304f22236491eb203521b0b570482007dbbf6ff
Jurabek <jurabek.azizkhujaev@gmail.com>
2019-11-14 23:43:00 +0100

    basket_api_build_problem_fixed
    Former-commit-id: 481a4caec07fe1b7a8b3553530373f5a3fe20c49
    Former-commit-id: 9ddd852c115f0f43d7566cf2340b5e61ae9a178c [formerly 17d472e6a6bd0be0930bdad992a1e48f6822286b]
    Former-commit-id: e6302e09f2e271b09798e065e6f3d8ffadf267ab

--- src/Server/services/basket.api/qa.sh
+++ src/Server/services/basket.api/qa.sh
@@ -3,4 +3,4 @@ CGO_ENABLED=0 go test ./controllers ./repositories ./oidc --cover -coverprofile=
 sonar-scanner -Dsonar.login=423f19c6b4a3ad087de2e7039311d3f047bcb90e \
     -Dsonar.pullrequest.base=$CI_EXTERNAL_PULL_REQUEST_TARGET_BRANCH_NAME \
     -Dsonar.pullrequest.branch=$CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_NAME \
-    -Dsonar.pullrequest.key=$CI_EXTERNAL_PULL_REQUEST_IID \
+    -Dsonar.pullrequest.key=$CI_EXTERNAL_PULL_REQUEST_IID

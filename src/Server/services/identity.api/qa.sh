c3a7b9e6f3b5da2212e867005e61218636f2deb7
Jurabek <jurabek.azizkhujaev@gmail.com>
2019-11-14 22:51:42 +0100

    Sonarqube Github PR update
    Former-commit-id: 97701581cb15c09455e1ac251f2d09d0f1e9a6c5
    Former-commit-id: 271088ebb1fdf2906dd87a6082174a0cfa8de1d0 [formerly 2170d93e4cb549a62670f4097ef637373c741aa8]
    Former-commit-id: 2d7207945e23caf54a3bd14983c9d01fa46f45e2

--- src/Server/services/identity.api/qa.sh
+++ src/Server/services/identity.api/qa.sh
@@ -16,7 +16,8 @@ dotnet-sonarscanner begin \
      /d:sonar.exclusions="Identity.API/wwwroot/**, Identity.API/Migrations/**"
      /d:sonar.pullRequest=$CI_EXTERNAL_PULL_REQUEST_IID \
      /d:sonar.github.repository=Jurabek/Restaurant-App \
-     /d:sonar.github.oauth=$GITHUB_SONAR_KEY
+     /d:sonar.github.oauth=$GITHUB_SONAR_KEY \
+     /d:sonar.pullrequest.github.endpoint=https://github.com/api/v3
 
 dotnet build Identity.API.sln
 dotnet-sonarscanner end /d:sonar.login="77a854f90e4e5cf4f26de587be88715750a2a9cc"

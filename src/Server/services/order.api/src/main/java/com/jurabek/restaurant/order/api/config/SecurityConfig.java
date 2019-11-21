d8fbd208d23a4342b92280683125eb5e5cbb8afd
Jurabek <jurabek.azizkhujaev@gmail.com>
2019-11-21 23:13:45 +0100

    Sonar scanner pre build docker image
    Former-commit-id: 0d9682753540149b4b7c1b0bd4fb8555286436c1
    Former-commit-id: 382dd36dc9f8429318903563e222d43438fc96c4 [formerly d5a4d31d65efe75742298819321aec8b70ce93c8]
    Former-commit-id: 2c6bb012aa63204c321145a04df7b05a1914fbd7

--- src/Server/services/order.api/src/main/java/com/jurabek/restaurant/order/api/config/SecurityConfig.java
+++ src/Server/services/order.api/src/main/java/com/jurabek/restaurant/order/api/config/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.jurabek.restaurant.order.api.config;
 
-import com.auth0.jwk.*;
-import com.auth0.spring.security.api.JwtAuthenticationProvider;
-import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -14,6 +12,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -28,106 +27,17 @@ import java.util.concurrent.TimeUnit;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Value(value = "${jwt.apiAudience}")
-    private String apiAudience;
-
-    @Value("${IDENTITY_URL_PUB:http://localhost:5100}")
-    private String identityUrl;
-
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        JwtWebSecurityConfigurer
-                .forRS256(apiAudience, "issuer")
-                .configure(http);
-        final JwkProvider jwkProvider = new JWKProviderBuilderCustom().build();
-        JwtWebSecurityConfigurer.forRS256(apiAudience, null,
-                new JwtAuthenticationProvider(jwkProvider, null, apiAudience))
-                .configure(http)
-                .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/v1/orders/getAll").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/api/v1/orders/**")
-                .authenticated();
-    }
-
-    class JWKProviderBuilderCustom {
-        private final int expiresIn;
-        private final TimeUnit expiresUnit;
-        private final int cacheSize;
-
-        JWKProviderBuilderCustom() {
-            this.expiresIn = 10;
-            this.expiresUnit = TimeUnit.HOURS;
-            this.cacheSize = 5;
-        }
-
-        JwkProvider build() {
-            JwkProvider urlProvider = null;
-            try {
-                urlProvider = new UrlJwkProviderOverride(
-                        new URL(identityUrl + "/.well-known/openid-configuration/jwks"));
-            } catch (MalformedURLException ignored) {
-            }
-            urlProvider = new GuavaCachedJwkProvider(urlProvider, cacheSize, expiresIn, expiresUnit);
-            return urlProvider;
-        }
-    }
-
-    static class UrlJwkProviderOverride implements JwkProvider {
-        final URL url;
-
-        UrlJwkProviderOverride(URL url) {
-            this.url = url;
-        }
-
-        private Map<String, Object> getJwks() throws SigningKeyNotFoundException {
-            try {
-                final URLConnection urlConnection = this.url.openConnection();
-                urlConnection.setRequestProperty("Accept", "application/json");
-                final InputStream inputStream = urlConnection.getInputStream();
-                final JsonFactory factory = new JsonFactory();
-                try (final JsonParser parser = factory.createParser(inputStream)) {
-                    final TypeReference<Map<String, Object>> typeReference = new TypeReference<Map<String, Object>>() {
-                    };
-                    return new ObjectMapper().reader().readValue(parser, typeReference);
-                }
-            } catch (IOException e) {
-                throw new SigningKeyNotFoundException("Cannot obtain jwks from url " + url.toString(), e);
-            }
-        }
-
-        List<Jwk> getAll() throws SigningKeyNotFoundException {
-            List<Jwk> jwks = Lists.newArrayList();
-            final List<Map<String, Object>> keys = (List<Map<String, Object>>) getJwks().get("keys");
-
-            if (keys == null || keys.isEmpty()) {
-                throw new SigningKeyNotFoundException("No keys found in " + url.toString(), null);
-            }
-
-            try {
-                for (Map<String, Object> values : keys) {
-                    jwks.add(Jwk.fromValues(values));
-                }
-            } catch (IllegalArgumentException e) {
-                throw new SigningKeyNotFoundException("Failed to parse jwk from json", e);
-            }
-            return jwks;
-        }
-
-        @Override
-        public Jwk get(String keyId) throws JwkException {
-            final List<Jwk> jwks = getAll();
-            if (keyId == null && jwks.size() == 1) {
-                return jwks.get(0);
-            }
-            if (keyId != null) {
-                for (Jwk jwk : jwks) {
-                    if (keyId.equals(jwk.getId())) {
-                        return jwk;
-                    }
-                }
-            }
-            throw new SigningKeyNotFoundException("No key found in " + url.toString() + " with kid " + keyId, null);
-        }
+        http.authorizeRequests().
+                antMatchers("/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**")
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 }
--- src/Server/services/order.api/src/main/java/com/jurabek/restaurant/order/api/controllers/OrdersController.java
+++ src/Server/services/order.api/src/main/java/com/jurabek/restaurant/order/api/controllers/OrdersController.java
@@ -6,6 +6,8 @@ import com.jurabek.restaurant.order.api.dtos.CustomerBasketDto;
 import com.jurabek.restaurant.order.api.dtos.CustomerOrderDto;
 import com.jurabek.restaurant.order.api.services.OrdersService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,26 +23,31 @@ public class OrdersController {
 	}
 
 	@GetMapping("getAllOrders")
+	@ApiOperation(value = "Returns all orders", authorizations= { @Authorization(value="oauth2") })
 	public List<CustomerOrderDto> getData() {
 		return this.ordersService.getAll();
 	}
 
 	@PostMapping()
+	@ApiOperation(value = "Creates new order", authorizations= { @Authorization(value="oauth2") })
 	public void create(@RequestBody CustomerBasketDto customerBasketDto) {
 		ordersService.Create(customerBasketDto);
 	}
 
 	@PutMapping()
+	@ApiOperation(value = "Updates order", authorizations= { @Authorization(value="oauth2") })
 	public void update(@RequestBody CustomerBasketDto customerBasketDto) {
 		ordersService.Update(customerBasketDto);
 	}
 
 	@GetMapping("getOrdersByCustomerId")
+	@ApiOperation(value = "Returns all orders for specific customer", authorizations= { @Authorization(value="oauth2") })
 	public List<CustomerOrderDto> getOrderByCustomerId(String customerId) {
 		return ordersService.getOrderByCustomerId(customerId);
 	}
 
 	@DeleteMapping()
+	@ApiOperation(value = "Deletes order", authorizations= { @Authorization(value="oauth2") })
 	public void delete(String orderId){
 	    ordersService.Delete(orderId);
     }

9c158a2d66a9f2a9db70b4024b857cdf2fb8bcf1
Tom Shen <tomshen0201@gmail.com>
2020-03-01 16:53:18 -0500

    Create react app with typescript. Add eslint and prettier config. - Issue #21

---
+++ src/Client/Restaurant.Client/Restaurant.Web/src/components/App/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './App.module.css';
+
+const App: React.StatelessComponent<{}> = () => {
+  return <div className={styles.container}>Hello World!</div>;
+};
+
+export default App;

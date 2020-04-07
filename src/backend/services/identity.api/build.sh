93955011570f3adea159b91209cedd7f0cb87558
jurabek <jurabek.azizkhujaev@gmail.com>
2020-04-07 23:37:34 +0200

    build failing issues

--- src/backend/services/identity.api/build.sh
+++ src/backend/services/identity.api/build.sh
@@ -2,6 +2,3 @@
 
 echo "Publishing Identity.API"
 dotnet publish Identity.API/Identity.API.csproj -c Release -o ./identity-api-release
-
-echo "Moving output to root folder"
-mv ./Identity.API/identity-api-release .

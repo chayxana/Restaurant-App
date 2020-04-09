b668ad99b3f2e086ad144af534a786e53ddacd8f
jurabek <jurabeka@pm.me>
2020-04-09 20:24:27 +0200

    docker image push for menu api and identity api

--- src/backend/services/identity.api/qa.sh
+++ src/backend/services/identity.api/qa.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 dotnet tool install --global dotnet-sonarscanner --version 4.8.0
-# chmod +x /root/.dotnet/tools/dotnet-sonarscanner
-
-# export PATH="${PATH}:/root/.dotnet/tools"
 
 dotnet restore Identity.API.sln
 
--- src/backend/services/identity.api/release.Dockerfile
+++ src/backend/services/identity.api/release.Dockerfile
@@ -1,6 +1,6 @@
 FROM mcr.microsoft.com/dotnet/core/aspnet:3.1-alpine
 WORKDIR /app
-COPY ./menu-api-release ./
+COPY ./identity-api-release ./
 
 EXPOSE 80
-CMD ["dotnet", "Menu.API.dll"]
+CMD ["dotnet", "Identity.API.dll"]

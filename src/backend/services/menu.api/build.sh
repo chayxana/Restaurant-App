c3b6aaea426925a04a20371c67983ae649b99ea8
jurabek <jurabek.azizkhujaev@gmail.com>
2020-04-09 23:08:46 +0200

    modified folder names and removed unnecessary scripts

--- src/backend/services/menu.api/build.sh
+++ src/backend/services/menu.api/build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+echo "Restoring Menu.API"
+dotnet restore Menu.API/Menu.API.csproj
+
 echo "Publishing Menu.API"
 dotnet publish Menu.API/Menu.API.csproj -c Release -o ./menu-api-release

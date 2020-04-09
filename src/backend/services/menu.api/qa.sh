6800bce6e8de047d4c792853e27fa5c93e0dadbe
jurabek <jurabek.azizkhujaev@gmail.com>
2020-04-09 12:08:34 +0200

    Menu api added Sonar Scanner

--- src/backend/services/menu.api/qa.sh
+++ src/backend/services/menu.api/qa.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dotnet tool install --global dotnet-sonarscanner --version 4.6.2
+# dotnet tool install --global dotnet-sonarscanner --version 4.6.2
 # chmod +x /root/.dotnet/tools/dotnet-sonarscanner
 
 # export PATH="${PATH}:/root/.dotnet/tools"
@@ -15,15 +15,12 @@ dotnet-sonarscanner begin \
     /d:"sonar.host.url=https://sonarcloud.io" \
     /o:"restaurant-app" \
     /k:"restaurant-menu-api" \
-    /d:"sonar.login=77a854f90e4e5cf4f26de587be88715750a2a9cc" \
+    /d:"sonar.login=$SONAR_TOKEN" \
     /d:sonar.cs.opencover.reportsPaths="Menu.API.UnitTests/coverage.opencover.xml" \
     /d:sonar.coverage.exclusions="**Tests*.cs" \
     /d:sonar.exclusions="Menu.API/Migrations/**, Menu.API/Data/**, Menu.API/Mapper/**"
     /d:sonar.github.repository=Jurabek/Restaurant-App \
-    /d:sonar.pullrequest.base=$CI_EXTERNAL_PULL_REQUEST_TARGET_BRANCH_NAME \
-    /d:sonar.pullrequest.branch=$CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_NAME \
-    /d:sonar.pullrequest.key=$CI_EXTERNAL_PULL_REQUEST_IID \
     /d:sonar.pullrequest.provider=GitHub
 
 dotnet build Menu.API.sln
-dotnet-sonarscanner end /d:sonar.login="77a854f90e4e5cf4f26de587be88715750a2a9cc"
+dotnet-sonarscanner end /d:sonar.login=$SONAR_TOKEN

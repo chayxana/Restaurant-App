78ae9c5e6bbfa7d918c77f2f2344dccf9384a7f9
jurabek <jurabek.azizkhujaev@gmail.com>
2020-05-08 00:09:49 +0200

    react web: github actions changed from matrix into node8

--- src/backend/web/web.client/web-app/src/store/reducers/foods.ts
+++ src/backend/web/web.client/web-app/src/store/reducers/foods.ts
@@ -1,19 +1,26 @@
 import { IFoodDto } from "src/api/dtos/FoodDto";
 import { combineReducers } from "redux";
-import { FOOD_RECIEVE } from '../constants';
+import { FOODS_RECIEVE, FOOD_RECIEVE } from '../constants';
 import { AllActions } from '../actions/actions';
 import { createReducer } from 'typesafe-actions';
 
 export interface FoodsState {
     foods: IFoodDto[];
+    food: IFoodDto;
 }
 
-const foods = createReducer([]).handleAction(FOOD_RECIEVE, (state: FoodsState, action: any) => {
+
+const foods = createReducer([]).handleAction(FOODS_RECIEVE, (state: FoodsState, action: any) => {
     return state.foods = action.payload;
 });
 
+const food = createReducer({}).handleAction(FOOD_RECIEVE, (state: FoodsState, action: any) => {
+    return state.food = action.payload;
+});
+
 export const foodsReducers = combineReducers<FoodsState, AllActions>({
-    foods
+    foods,
+    food
 });
 
 export const getFoods = (state: FoodsState) => state.foods;

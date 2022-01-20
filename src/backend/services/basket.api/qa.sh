e8474242c83f61a8480c09479d6b271d2942b63d
Jurabek <jurabek.azizkhujaev@gmail.com>
2022-01-20 20:42:50 +0100

    Docker compose basket api #85 (#86)
    * Docker-compose basket-api #85
    
    * order-api github actions fixed for bumped version

--- src/backend/services/basket.api/qa.sh
+++ src/backend/services/basket.api/qa.sh
@@ -1,4 +1,4 @@
-CGO_ENABLED=0 go test ./controllers ./repositories ./oidc --cover -coverprofile=coverage.out
+CGO_ENABLED=0 go test ./handlers ./repositories ./oidc --cover -coverprofile=coverage.out
 # sonar-scanner -Dsonar.login=423f19c6b4a3ad087de2e7039311d3f047bcb90e \
 #     -Dsonar.pullrequest.base=$CI_EXTERNAL_PULL_REQUEST_TARGET_BRANCH_NAME \
 #     -Dsonar.pullrequest.branch=$CI_EXTERNAL_PULL_REQUEST_SOURCE_BRANCH_NAME \
--- src/backend/services/basket.api/test.sh
+++ src/backend/services/basket.api/test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-CGO_ENABLED=0 go test ./controllers ./repositories ./oidc --cover -coverprofile=coverage.out | tee output.txt
+CGO_ENABLED=0 go test ./handlers ./repositories ./oidc --cover -coverprofile=coverage.out | tee output.txt
 go tool cover -html=coverage.out -o ./reports/coverage.html

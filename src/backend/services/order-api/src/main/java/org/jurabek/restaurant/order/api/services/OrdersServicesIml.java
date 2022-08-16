b8bf44aec16d9634798a8f1b273345ffc82c8cf3
jurabek <jurabek.azizkhujaev@gmail.com>
2022-08-17 01:24:49 +0200

    fixing tests

--- src/backend/services/order-api/src/main/java/org/jurabek/restaurant/order/api/services/OrdersServicesIml.java
+++ src/backend/services/order-api/src/main/java/org/jurabek/restaurant/order/api/services/OrdersServicesIml.java
@@ -1,19 +1,17 @@
 package org.jurabek.restaurant.order.api.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import org.jurabek.restaurant.order.api.dtos.CustomerBasketDto;
 import org.jurabek.restaurant.order.api.dtos.CustomerOrderDto;
-import org.jurabek.restaurant.order.api.models.Order;
+import org.jurabek.restaurant.order.api.mappers.OrdersMapper;
 import org.jurabek.restaurant.order.api.models.OrderItems;
 import org.jurabek.restaurant.order.api.repositories.OrdersRepository;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeToken;
 
 /**
  * OrdersServicesIml
@@ -22,17 +20,17 @@ import org.modelmapper.TypeToken;
 public class OrdersServicesIml implements OrdersService {
 
     private final OrdersRepository ordersRepository;
-    private final ModelMapper modelMapper;
+    private final OrdersMapper mapper;
 
     @Inject
-    public OrdersServicesIml(OrdersRepository ordersRepository, ModelMapper modelMapper) {
+    public OrdersServicesIml(OrdersRepository ordersRepository, OrdersMapper mapper) {
         this.ordersRepository = ordersRepository;
-        this.modelMapper = modelMapper;
+        this.mapper = mapper;
     }
 
     @Override
     public void Create(CustomerBasketDto customerBasketDto) {
-        var order = modelMapper.map(customerBasketDto, Order.class);
+        var order = mapper.mapDtoToOrder(customerBasketDto);
         for (OrderItems orderItems : order.getOrderItems()) {
             orderItems.setOrder(order);
         }
@@ -41,10 +39,11 @@ public class OrdersServicesIml implements OrdersService {
 
     @Override
     public List<CustomerOrderDto> getAll() {
-        var orders = this.ordersRepository.fetchAll();
-        var orderDtoType = new TypeToken<ArrayList<CustomerOrderDto>>() {}.getType();
-        return modelMapper.map(orders, orderDtoType);
-	}
+        return this.ordersRepository.fetchAll()
+                .stream()
+                .map(o -> mapper.mapOrderToDto(o))
+                .collect(Collectors.toList());
+    }
 
     @Override
     public void Update(CustomerBasketDto customerBasketDto) {
@@ -52,19 +51,20 @@ public class OrdersServicesIml implements OrdersService {
 
     @Override
     public List<CustomerOrderDto> getOrderByCustomerId(String customerId) {
-        var order = ordersRepository.getByBuyerId(UUID.fromString(customerId));
-        var orderDtoType = new TypeToken<ArrayList<CustomerOrderDto>>() {}.getType();
-        return modelMapper.map(order, orderDtoType);
+        return ordersRepository.getByBuyerId(UUID.fromString(customerId))
+                .stream()
+                .map(o -> mapper.mapOrderToDto(o))
+                .collect(Collectors.toList());
     }
 
-	@Override
-	public void Delete(String orderId) {
+    @Override
+    public void Delete(String orderId) {
         ordersRepository.deleteById(UUID.fromString(orderId));
-	}
+    }
 
     @Override
     public CustomerOrderDto getById(String orderId) {
         var order = ordersRepository.findById(UUID.fromString(orderId));
-        return modelMapper.map(order, CustomerOrderDto.class);
+        return mapper.mapOrderToDto(order);
     }
 }
--- src/backend/services/order-api/src/test/java/org/jurabek/restaurant/order/api/services/OrdersServiceTests.java
+++ src/backend/services/order-api/src/test/java/org/jurabek/restaurant/order/api/services/OrdersServiceTests.java
@@ -5,26 +5,22 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-// import java.lang.reflect.Type;
-// import java.util.ArrayList;
-// import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
-
-import javax.inject.Inject;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.jurabek.restaurant.order.api.dtos.CustomerBasketDto;
 import org.jurabek.restaurant.order.api.dtos.CustomerOrderDto;
+import org.jurabek.restaurant.order.api.mappers.OrdersMapper;
 import org.jurabek.restaurant.order.api.models.Order;
 import org.jurabek.restaurant.order.api.models.OrderItems;
 import org.jurabek.restaurant.order.api.repositories.OrdersRepository;
-import org.mockito.Mockito;
-import org.modelmapper.ModelMapper;
-// import org.modelmapper.TypeToken;
 
-import io.quarkus.test.junit.QuarkusMock;
+import com.google.inject.Inject;
+
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectMock;
 
@@ -35,84 +31,77 @@ public class OrdersServiceTests {
     private OrdersRepository ordersRepository;
 
     @InjectMock
-    private ModelMapper modelMapper;
+    private OrdersMapper mapper;
 
+    @Inject
     private OrdersServicesIml ordersService;
 
     @BeforeEach
     public void setup() {
-        ordersService = new OrdersServicesIml(ordersRepository, modelMapper);
+        ordersService = new OrdersServicesIml(ordersRepository, mapper);
     }
 
-    // @Test
-    // public void CreateShouldCreateWhenCustomerBasketDto() {
-    //     var customerBasketDto = new CustomerBasketDto();
-    //     var order = new Order();
-    //     var orderItems = new ArrayList<OrderItems>();
-    //     orderItems.add(new OrderItems());
-    //     order.setOrderItems(orderItems);
+    @Test
+    public void CreateShouldCreateWhenCustomerBasketDto() {
+        var customerBasketDto = new CustomerBasketDto();
+        var order = new Order();
+        var orderItems = new ArrayList<OrderItems>();
+        orderItems.add(new OrderItems());
+        order.setOrderItems(orderItems);
 
-    //     Mockito.when(modelMapper.map(customerBasketDto, Order.class)).thenReturn(order);
+        when(mapper.mapDtoToOrder(customerBasketDto)).thenReturn(order);
 
-    //     ordersService.Create(customerBasketDto);
+        ordersService.Create(customerBasketDto);
 
-    //     Mockito.verify(ordersRepository, times(1)).persist(order);
-    // }
+        verify(ordersRepository, times(1)).persist(order);
+    }
 
-    // @Test
-    // public void GetAllShouldReturnAllOrderDtos() {
-    //     List<Order> orders = new ArrayList<>();
-    //     orders.add(new Order());
+    @Test
+    public void GetAllShouldReturnAllOrderDtos() {
+        var order = new Order();
+        var orders = List.of(order);
 
-    //     List<CustomerOrderDto> dtos = new ArrayList<>();
-    //     dtos.add(new CustomerOrderDto());
+        var dto = new CustomerOrderDto();
 
-    //     Type orderDtoType = new TypeToken<ArrayList<CustomerOrderDto>>() {
-    //     }.getType();
+        when(ordersRepository.fetchAll()).thenReturn(orders);
+        when(mapper.mapOrderToDto(order)).thenReturn(dto);
 
-    //     when(ordersRepository.findAll().list()).thenReturn(orders);
-    //     when(modelMapper.map(orders, orderDtoType)).thenReturn(dtos);
+        List<CustomerOrderDto> result = ordersService.getAll();
 
-    //     List<CustomerOrderDto> result = ordersService.getAll();
+        Assertions.assertEquals(1, result.size());
+        Assertions.assertEquals(result.get(0), dto);
+    }
 
-    //     Assertions.assertEquals(1, result.size());
-    //     Assertions.assertEquals(result.get(0), dtos.get(0));
-    // }
+    @Test
+    public void GivenCustomerIdReturnOrder() {
+        var order = new Order();
+        var orders = List.of(order);
+        String customerId = UUID.randomUUID().toString();
+        var dto = new CustomerOrderDto();
 
-    // @Test
-    // public void GivenCustomerIdReturnOrder() {
-    //     List<Order> orders = new ArrayList<>();
-    //     orders.add(new Order());
-    //     String customerId = UUID.randomUUID().toString();
-    //     List<CustomerOrderDto> dtos = new ArrayList<>();
-    //     dtos.add(new CustomerOrderDto());
+        when(ordersRepository.getByBuyerId(UUID.fromString(customerId))).thenReturn(orders);
 
-    //     Type orderDtoType = new TypeToken<ArrayList<CustomerOrderDto>>() {
-    //     }.getType();
+        when(mapper.mapOrderToDto(order)).thenReturn(dto);
 
-    //     when(ordersRepository.getByBuyerId(UUID.fromString(customerId))).thenReturn(orders);
+        List<CustomerOrderDto> result = ordersService.getOrderByCustomerId(customerId);
 
-    //     when(modelMapper.map(orders, orderDtoType)).thenReturn(dtos);
+        Assertions.assertEquals(result.size(), 1);
+        Assertions.assertEquals(result.get(0), dto);
+    }
 
-    //     List<CustomerOrderDto> result = ordersService.getOrderByCustomerId(customerId);
+    @Test
+    public void delete_test() {
+        // arrange
+        String id = UUID.randomUUID().toString();
+        UUID idUUID = UUID.fromString(id);
+        when(ordersRepository.deleteById(idUUID)).thenReturn(true);
 
-    //     Assertions.assertEquals(result.size(), 1);
-    //     Assertions.assertEquals(result.get(0), dtos.get(0));
-    // }
+        // act
+        ordersService.Delete(id);
 
-    // @Test
-    // public void delete_test() {
-    //     // arrange
-    //     String id = UUID.randomUUID().toString();
-    //     UUID idUUID = UUID.fromString(id);
-    //     doNothing().when(ordersRepository).deleteById(idUUID);
-
-    //     // act
-    //     ordersService.Delete(id);
-
-    //     // assert
-    //     verify(ordersRepository, times(1)).deleteById(idUUID);
-    // }
+        // assert
+        verify(ordersRepository, times(1)).deleteById(idUUID);
+    }
 
     @Test
     public void getById_test() {
@@ -121,7 +110,7 @@ public class OrdersServiceTests {
         var order = new Order();
         var orderDto = new CustomerOrderDto();
         when(ordersRepository.findById(UUID.fromString(orderId))).thenReturn(order);
-        when(modelMapper.map(order, CustomerOrderDto.class)).thenReturn(orderDto);
+        when(mapper.mapOrderToDto(order)).thenReturn(orderDto);
 
         // act
         var result = ordersService.getById(orderId);

452fa57edc0b6142d488084c57fe5010df1b9bbe
jurabek <jurabek.azizkhujaev@gmail.com>
2022-10-31 21:46:54 +0100

    adding DB into Rust application

---
+++ src/backend/services/catalog-api/migrations/2022-10-23-201152_category/up.sql
@@ -0,0 +1,5 @@
+-- Your SQL goes here
+CREATE TABLE category(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL
+);
---
+++ src/backend/services/catalog-api/src/handlers/category.rs
@@ -0,0 +1,23 @@
+use crate::models::category::{Category};
+
+use rocket::serde::json::Json;
+use rocket::response::{Debug};
+
+use crate::db::db::establish_connection;
+use crate::schema::{self};
+
+use diesel::{prelude::*};
+
+type Result<T, E = Debug<diesel::result::Error>> = std::result::Result<T, E>;
+
+#[get("/", format = "json")]
+pub fn get_categories() -> Result<Json<Vec<Category>>> {
+    use schema::category::dsl::*;
+
+    let connection = &mut establish_connection();
+    let res = category
+    .load::<Category>(connection)
+    .expect("failed to loading catalogs");
+
+    return Ok(Json(res));
+}

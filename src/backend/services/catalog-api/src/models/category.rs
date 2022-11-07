2d3ec9a5d59911ddf5f14d225705259e1aa35896
jurabek <jurabeka@pm.me>
2022-11-07 15:50:12 +0100

    fix rust warnings

--- src/backend/services/catalog-api/src/models/category.rs
+++ src/backend/services/catalog-api/src/models/category.rs
@@ -3,15 +3,15 @@ use diesel::{prelude::*};
 use crate::schema::category;
 use rocket_okapi::{JsonSchema};
 
-#[diesel(table_name = category)]
 #[derive(Serialize, Deserialize, Debug, Queryable, JsonSchema)]
+#[diesel(table_name = category)]
 pub struct Category {
     pub id: i32,
     pub name: String,
 }
 
-#[diesel(table_name = category)]
 #[derive(Serialize, Deserialize, Debug, Insertable, JsonSchema)]
+#[diesel(table_name = category)]
 pub struct NewCategory {
     pub name: String
 }

224d938b2844f748f659b1e51c5d834dae505db8
jurabek <jurabeka@pm.me>
2023-01-19 23:19:43 +0100

    Upgrading quarkus and remove getter setters from some classed prior to Lombok

--- src/backend/services/order-api/src/main/java/org/jurabek/restaurant/order/api/dtos/CustomerOrderDto.java
+++ src/backend/services/order-api/src/main/java/org/jurabek/restaurant/order/api/dtos/CustomerOrderDto.java
@@ -4,53 +4,16 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * OrderDto
  */
+@Getter
+@Setter
 public class CustomerOrderDto {
     private UUID id;
     private Date orderedDate;
     private List<CustomerOrderItemsDto> orderItems;
-
-    /**
-     * @return the id
-     */
-    public UUID getId() {
-        return id;
-    }
-
-    /**
-	 * @return the orderItems
-	 */
-	public List<CustomerOrderItemsDto> getOrderItems() {
-		return orderItems;
-	}
-
-	/**
-	 * @param orderItems the orderItems to set
-	 */
-	public void setOrderItems(List<CustomerOrderItemsDto> orderItems) {
-		this.orderItems = orderItems;
-	}
-
-	/**
-     * @return the orderedDate
-     */
-    public Date getOrderedDate() {
-        return orderedDate;
-    }
-
-    /**
-     * @param orderedDate the orderedDate to set
-     */
-    public void setOrderedDate(Date orderedDate) {
-        this.orderedDate = orderedDate;
-    }
-
-    /**
-     * @param id the id to set
-     */
-    public void setId(UUID id) {
-        this.id = id;
-    }
 }
--- src/backend/services/order-api/src/main/java/org/jurabek/restaurant/order/api/dtos/CustomerOrderItemsDto.java
+++ src/backend/services/order-api/src/main/java/org/jurabek/restaurant/order/api/dtos/CustomerOrderItemsDto.java
@@ -2,53 +2,18 @@ package org.jurabek.restaurant.order.api.dtos;
 
 import java.util.UUID;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * OrderItemsDto
  */
+@Getter
+@Setter
 public class CustomerOrderItemsDto {
     private UUID id;
     private UUID foodId;
     private float unitPrice;
     private float units;
     private String foodName;
-
-    public UUID getId() {
-        return this.id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public UUID getFoodId() {
-        return this.foodId;
-    }
-
-    public void setFoodId(UUID foodId) {
-        this.foodId = foodId;
-    }
-
-    public float getUnitPrice() {
-        return this.unitPrice;
-    }
-
-    public void setUnitPrice(float unitPrice) {
-        this.unitPrice = unitPrice;
-    }
-
-    public float getUnits() {
-        return this.units;
-    }
-
-    public void setUnits(float units) {
-        this.units = units;
-    }
-
-    public String getFoodName() {
-        return this.foodName;
-    }
-
-    public void setFoodName(String foodName) {
-        this.foodName = foodName;
-    }
 }

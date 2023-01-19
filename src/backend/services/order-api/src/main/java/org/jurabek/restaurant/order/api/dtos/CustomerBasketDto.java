a14ae9725ca397da0c1b0d112c66c06eee7b650b
jurabek <8397056+jurabek@users.noreply.github.com>
2023-01-19 23:19:43 +0100

    Upgrading quarkus and remove getter setters from some classed prior to Lombok

--- src/backend/services/order-api/src/main/java/org/jurabek/restaurant/order/api/dtos/CustomerBasketDto.java
+++ src/backend/services/order-api/src/main/java/org/jurabek/restaurant/order/api/dtos/CustomerBasketDto.java
@@ -3,42 +3,16 @@ package org.jurabek.restaurant.order.api.dtos;
 import java.util.List;
 import java.util.UUID;
 
+import lombok.Getter;
+import lombok.Setter;
+
 
 /**
  * CustomerBasketDto
  */
-
+@Getter
+@Setter
 public class CustomerBasketDto {
-
     private UUID customerId;
-    
     private List<CustomerBasketItemDto> items;
-
-    /**
-     * @return the items
-     */
-    public List<CustomerBasketItemDto> getItems() {
-        return items;
-    }
-
-    /**
-     * @return the customerId
-     */
-    public UUID getCustomerId() {
-        return customerId;
-    }
-
-    /**
-     * @param customerId the customerId to set
-     */
-    public void setCustomerId(UUID customerId) {
-        this.customerId = customerId;
-    }
-
-    /**
-     * @param items the items to set
-     */
-    public void setItems(List<CustomerBasketItemDto> items) {
-        this.items = items;
-    }
 }

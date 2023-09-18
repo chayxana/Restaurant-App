5b146308830a3e7f047931daedd10dd8c3b395ca
jurabek <jurabeka@pm.me>
2023-09-18 09:46:07 +0200

    fix docker run

--- src/backend/docker/run.sh
+++ src/backend/docker/run.sh
@@ -1,6 +1,7 @@
-docker-compose -f docker/docker-compose.yml \
-    -f docker/docker-compose.override.yml \
+#!/bin/sh
+
+docker-compose -f docker/docker-compose.yml -f docker/docker-compose.override.yml \
     -f docker/docker-compose.traefik.yml \
     -f docker/docker-compose.kafka.yml \
     -f docker/docker-compose.grafana.yaml \
-    -f docker/docker-compose.otel.yml up -d --build
+    -f docker/docker-compose.otel.yml --project-directory . up -d --build

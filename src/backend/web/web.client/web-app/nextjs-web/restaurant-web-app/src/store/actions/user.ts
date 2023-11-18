04751c7579f8496e2971b94414458b6f37641fab
jurabek <jurabeka@pm.me>
2023-11-18 16:57:28 +0100

    removing next.js experiment

--- src/backend/web/web.client/web-app/nextjs-web/restaurant-web-app/src/store/actions/user.ts
+++
@@ -1,9 +0,0 @@
-import { action } from "typesafe-actions";
-import { Dispatch } from 'redux';
-import { LOG_OUT } from '../constants';
-
-export const logOut = () => (dispatch: Dispatch) => {
-    dispatch(requestLogOut()); 
-};
-
-export const requestLogOut = () => action(LOG_OUT);

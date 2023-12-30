20eb4647ced904b153bed5a8e8f31f4552200099
jurabek <jurabeka@pm.me>
2023-12-30 15:17:50 +0100

    fixing checkout-api packages incompatibility

--- src/backend/services/checkout-api/src/tracer.ts
+++ src/backend/services/checkout-api/src/tracer.ts
@@ -1,18 +1,10 @@
-import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
-import { BatchSpanProcessor, SimpleSpanProcessor, ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base';
-import { registerInstrumentations } from '@opentelemetry/instrumentation';
-import { GrpcInstrumentation } from '@opentelemetry/instrumentation-grpc';
-import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
-import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
-import { trace } from '@opentelemetry/api';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc';
 import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
-import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 import { KafkaJsInstrumentation } from 'opentelemetry-instrumentation-kafkajs';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { ConsoleMetricExporter, PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
 
 export const OpenTelemetry = (serviceName: string) => {
@@ -25,13 +17,19 @@ export const OpenTelemetry = (serviceName: string) => {
     })
   );
 
-  const processor = new BatchSpanProcessor(new OTLPTraceExporter({
+  const traceExporter = new OTLPTraceExporter({
     url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
-  }));
+  });
+
+  const metricReader = new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({
+      url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+    }),
+  });
 
   return new NodeSDK({
     resource,
-    spanProcessor: processor,
+    traceExporter,
     instrumentations: [
       getNodeAutoInstrumentations({
         '@opentelemetry/instrumentation-fs': {
@@ -40,10 +38,6 @@ export const OpenTelemetry = (serviceName: string) => {
       }),
       new KafkaJsInstrumentation()
     ],
-    metricReader: new PeriodicExportingMetricReader({
-      exporter: new OTLPMetricExporter({
-        url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
-      }),
-    }),
+    metricReader,
   })
 }

888eed4ea164afd5f4db697a39f9193da6f580bd
jurabek <jurabeka@pm.me>
2024-01-21 02:12:07 +0100

    Removing old react app prior to new Next.js version

--- src/backend/web/web.client/web-app/src/store/actions/auth.ts
+++
@@ -1,4 +0,0 @@
-import { createAction } from 'typesafe-actions';
-import { RECIEVE_TOKEN } from '../constants';
-
-export const recieveToken = () => createAction(RECIEVE_TOKEN);
--- src/backend/web/web.client/web-app/src/store/reducers/selectors.ts
+++
@@ -1,4 +0,0 @@
-import { RootState } from './redusers';
-import * as foods from './foods';
-
-export const getFoods = (state: RootState) => foods.getFoods(state.foods); 

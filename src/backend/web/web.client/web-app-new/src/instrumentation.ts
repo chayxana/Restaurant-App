b4fdbe838916debd7348e066ca09f4f76c1be5bd
jurabek <jurabeka@pm.me>
2024-02-02 00:40:18 +0100

    Adding OTEL Instrumentation for the Next.js app

---
+++ src/backend/web/web.client/web-app-new/src/instrumentation.ts
@@ -0,0 +1,27 @@
+import { NodeSDK } from '@opentelemetry/sdk-node';
+// import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
+import {
+    PeriodicExportingMetricReader,
+    ConsoleMetricExporter,
+} from '@opentelemetry/sdk-metrics';
+import { Resource } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+
+import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
+
+const sdk = new NodeSDK({
+    resource: new Resource({
+        [SemanticResourceAttributes.SERVICE_NAME]: 'web-app',
+        [SemanticResourceAttributes.SERVICE_VERSION]: '1.0',
+    }),
+    traceExporter: new OTLPTraceExporter({
+        url: 'http://localhost:4318/v1/traces',
+    }),
+    metricReader: new PeriodicExportingMetricReader({
+        exporter: new ConsoleMetricExporter(),
+    }),
+});
+
+sdk.start();
